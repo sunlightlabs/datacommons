46682a0424264b0cbcc45c2dd1a2a5de6db80ec9
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2009-10-22 15:28:13 -0400

    comments and formatting. [#6]

--- dc_matchbox/matchbox/search.py
+++ dc_matchbox/matchbox/search.py
@@ -4,7 +4,7 @@ def entity_search(connection, query):
     """
     Search for all entities with a normalized name prefixed by the normalized query string.
     
-    Returns an iterator over a triple (id, name, count)
+    Returns an iterator over triples (id, name, count).
     """
     
     stmt = "select e.id, e.name, count(*) \
@@ -19,6 +19,13 @@ def entity_search(connection, query):
 transaction_result_columns = ['Contrib', 'State', 'Orgname', 'Emp_EF', 'FecOccEmp', 'Date', 'Amount']
 
 def transaction_search(connection, entity_id, result_columns=transaction_result_columns):
+    """
+    Search for all transactions that belong to a particular entity.
+    
+    Note: once transactions have donor and recipient entities in addition to employer entities
+    this function will have to be adapted.
+    """
+    
     stmt = "select %(result_columns)s \
             from individual_contributions \
             where employer_entity_id = %(entity_id)s \
