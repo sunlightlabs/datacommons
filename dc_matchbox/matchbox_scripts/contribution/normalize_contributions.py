867b5e517d6b8ce90eb63532d9419835426eaa03
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2009-10-28 16:50:57 -0400

    Updated normalization scripts to use Django's DB connection, rather than getting one from MySQLdb. Makes configuration easier, since settings come from settings.py.

--- dc_matchbox/matchbox_scripts/contribution/normalize_contributions.py
+++ dc_matchbox/matchbox_scripts/contribution/normalize_contributions.py
@@ -1,19 +1,13 @@
 
-import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 """
 Create the string normalization tables for a database.
 Relies on: contribution_organization_name column in contribution_contribution table.
 """
 
-import MySQLdb
 from strings.normalizer import basic_normalizer
-from scripts.support.normalize_database import normalize
-from settings import DATABASE_NAME, DATABASE_USER, DATABASE_HOST
+from matchbox_scripts.support.normalize_database import normalize
 
+columns = [('entityalias', ['entityalias_alias'])]
 
-connection = MySQLdb.connect(host=DATABASE_HOST, user=DATABASE_USER, db=DATABASE_NAME)
-columns = [('contribution', ['contribution_organization_name'])]
-
-normalize(connection, columns, basic_normalizer)
+normalize(columns, basic_normalizer)
