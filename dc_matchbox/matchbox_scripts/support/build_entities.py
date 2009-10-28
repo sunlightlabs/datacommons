0adc217788525139d67b81a5926710a9547ca4db
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2009-10-28 14:53:36 -0400

    Added second implementation of build_entities.create_entity that uses raw SQL. Switch between two implementations with the USE_RAW variable. Set to False by default. [#31]

--- dc_matchbox/matchbox_scripts/support/build_entities.py
+++ dc_matchbox/matchbox_scripts/support/build_entities.py
@@ -1,13 +1,19 @@
 
+USE_RAW = False
 
-from datetime import datetime
 
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
-from django.db import connection
+if USE_RAW:
+    import MySQLdb
+    connection = MySQLdb.Connect(host='localhost', db='datacommons', user='root')
+else:
+    from django.db import connection
 
-from matchbox.models import Entity, entity_types , EntityAttribute, EntityAlias
+
+from datetime import datetime
+from matchbox.models import Entity, entity_types , EntityAttribute, EntityAlias, sql_names
 
 def quote(value):
     return value.replace("\\","\\\\").replace("'","\\'")
@@ -62,6 +68,47 @@ def populate_entities(transaction_table, entity_name_column, entity_id_column, a
         
         return e.id
 
+    def create_entity_raw(name):
+        stmt = "insert into `%(entity)s` \
+                (`%(entity_name)s`, `%(entity_type)s`, `%(entity_reviewer)s`, `%(entity_timestamp)s`, `%(entity_notes)s`) \
+                values (%%s, %%s, %%s, %%s, %%s)" % \
+                sql_names
+        cursor.execute(stmt, [name, type, reviewer, timestamp, ''])
+        entity_id = connection.insert_id()
+        
+        # create aliases
+        aliases = set()
+        for alias_column in alias_columns:
+            stmt = "select distinct `%s` from `%s` where `%s` = %%s" % \
+                    (alias_column, transaction_table, entity_name_column)
+            cursor.execute(stmt,[name])
+            for (alias,) in cursor:
+                aliases.add(alias)
+        for alias in aliases:
+            if alias.strip():
+                stmt = "insert into `%(entityalias)s` \
+                        (`%(entityalias_entity)s`, `%(entityalias_alias)s`) \
+                        values (%%s, %%s)" % \
+                        sql_names
+                cursor.execute(stmt,[entity_id, alias])
+            
+        # create attributes
+        attributes = set()
+        for attribute_column in attribute_columns:
+            stmt = "select distinct `%s` from `%s` where `%s` = %%s" % \
+                    (attribute_column, transaction_table, entity_name_column)
+            cursor.execute(stmt,[name])
+            for (attribute,) in cursor:
+                attributes.add(attribute)
+        for attribute in attributes:
+            if attribute.strip():
+                stmt = "insert into `%(entityattribute)s` \
+                        (`%(entityattribute_entity)s`, `%(entityattribute_namespace)s`, `%(entityattribute_value)`) \
+                        values (%%s, %%s, %%s)" % \
+                        sql_names
+                cursor.execute(stmt,[entity_id, attribute_namespace, attribute])
+        
+        return entity_id
 
 
     def update_transactions(name, id):
@@ -71,8 +118,8 @@ def populate_entities(transaction_table, entity_name_column, entity_id_column, a
     
     for (name,) in retrieve_names():
         if name:
-            print("creating entity '%s'" % name)
-            id = create_entity(name)
+            #print("creating entity '%s'" % name)
+            id = create_entity_raw(name) if USE_RAW else create_entity(name)
             update_transactions(name, id)
     
 
