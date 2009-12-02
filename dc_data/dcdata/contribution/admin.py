749989ac5566545c98732f8baabc2086c3d3f405
Jeremy Carbaugh <jcarbaugh@gmail.com>
2009-12-02 17:58:26 -0500

    initial app layout, views, and a semi-working api

--- dc_data/dcdata/contribution/admin.py
+++ dc_data/dcdata/contribution/admin.py
@@ -3,6 +3,6 @@ from dcdata.contribution.models import Contribution
 
 class ContributionAdmin(admin.ModelAdmin):
     list_display = ('datestamp','contributor_name','contributor_city','contributor_state','amount','recipient_type','recipient_name','organization_name','committee_name','seat','seat_status')
-    list_filter = ('recipient_type','seat')
+    #list_filter = ('recipient_type','seat')
 
 admin.site.register(Contribution, ContributionAdmin)
