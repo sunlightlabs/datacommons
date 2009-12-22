99b3916b0bc3729f383ebc5cd4de8da810231efe
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2009-12-22 13:30:07 -0500

    Upped limits on innermost query to 1000. The planner wasn't using the index with a lower limit. Put limit of 50 on number of entities, otherwise planner was using sequential scan. [#98]

--- dc_matchbox/matchbox/queries.py
+++ dc_matchbox/matchbox/queries.py
@@ -14,13 +14,14 @@ from strings.normalizer import basic_normalizer
 
 _entities_by_name_query = "select distinct e.id, e.name \
                             from \
-                                ((select distinct %(normalization_original)s from %(normalization)s where %(normalization_normalized)s like %%s limit 500)\
+                                ((select distinct %(normalization_original)s from %(normalization)s where %(normalization_normalized)s like %%s limit 1000)\
                                  union distinct \
                                 (select distinct %(normalization_original)s from %(normalization)s \
-                                    where to_tsvector('simple', %(normalization_original)s) @@ to_tsquery('simple', %%s) limit 500)) n \
+                                    where to_tsvector('simple', %(normalization_original)s) @@ to_tsquery('simple', %%s) limit 1000)) n \
                             inner join %(entityalias)s a on a.%(entityalias_alias)s = n.%(normalization_original)s \
                             inner join %(entity)s e on e.%(entity_id)s = a.%(entityalias_entity)s \
-                            where e.%(entity_type)s = %%s" % sql_names
+                            where e.%(entity_type)s = %%s \
+                            limit 50" % sql_names
 
 def search_entities_by_name(query, type_filter):
     """
