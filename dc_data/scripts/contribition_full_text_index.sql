5af0950d095706e13233ea7c79a387c1791db60e
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2010-01-19 11:58:17 -0500

    Updated tests to show that stop word removal is working. [#125 state:resolved]

--- dc_data/scripts/contribition_full_text_index.sql
+++ dc_data/scripts/contribition_full_text_index.sql
@@ -1,5 +1,15 @@
---create index contribution_contribution_contributor_name on contribution_contribution using gin(to_tsvector('simple', contributor_name));
-create index contribution_contribution_organization_name on contribution_contribution using gin(to_tsvector('simple', organization_name));
-create index contribution_contribution_parent_organization_name on contribution_contribution using gin(to_tsvector('simple', parent_organization_name));
-create index contribution_contribution_committee_name on contribution_contribution using gin(to_tsvector('simple', committee_name));
-create index contribution_contribution_recipient_name on contribution_contribution using gin(to_tsvector('simple', recipient_name));
+create text search dictionary datacommons ( template = simple, stopwords = datacommons );
+create text search configuration datacommons ( copy = simple );
+alter text search configuration datacommons alter mapping for asciiword with datacommons;
+
+drop index if exists contribution_contribution_contributor_name;
+drop index if exists contribution_contribution_organization_name;
+drop index if exists contribution_contribution_parent_organization_name;
+drop index if exists contribution_contribution_committee_name;
+drop index if exists contribution_contribution_recipient_name;
+
+create index contribution_contribution_contributor_name on contribution_contribution using gin(to_tsvector('datacommons', contributor_name));
+create index contribution_contribution_organization_name on contribution_contribution using gin(to_tsvector('datacommons', organization_name));
+create index contribution_contribution_parent_organization_name on contribution_contribution using gin(to_tsvector('datacommons', parent_organization_name));
+create index contribution_contribution_committee_name on contribution_contribution using gin(to_tsvector('datacommons', committee_name));
+create index contribution_contribution_recipient_name on contribution_contribution using gin(to_tsvector('datacommons', recipient_name));
