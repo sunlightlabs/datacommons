fcdec498c15f05f8c3b2338f677f38a5db5c485f
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2010-01-23 12:37:00 -0500

    Changed entity criteria to include all five name and urn fields. If we later find out that some of them don't match anything, then we can remove them for efficiency reasons. [#127]

--- dc_matchbox/matchbox_scripts/contribution/build_big_hitters.py
+++ dc_matchbox/matchbox_scripts/contribution/build_big_hitters.py
@@ -17,16 +17,23 @@ def build_big_hitters(csv_rows):
         crp_urn = contributor_urn(crp_id.strip())
         nimsp_urn = 'urn:nimsp:contributor:' + nimsp_id.strip()
         clean_name = name.strip().decode('utf8', 'replace')
-        return (clean_name, 'organization', [('contributor_name', clean_name, 'contributor_entity'),
-                                       ('organization_name', clean_name, 'organization_entity'),
-                                       ('parent_organization_name', clean_name, 'parent_organization_entity'),
-                                       ('contributor_urn', nimsp_urn, 'contributor_entity'),
-                                       ('organization_urn', nimsp_urn, 'organization_entity'),
-                                       ('parent_organization_urn', nimsp_urn, 'parent_organization_entity'),
-                                       ('contributor_urn', crp_urn, 'contributor_entity')])
         
+        columns = ['contributor', 'organization', 'parent_organization', 'committee', 'recipient']
+        name_columns = [column + '_name' for column in columns]
+        urn_columns = [column + '_urn' for column in columns]
+        entity_columns = [column + '_entity' for column in columns]
+        
+        name_criteria = zip(name_columns, [clean_name] * 5, entity_columns)
+        crp_urn_criteria = zip(urn_columns, [crp_urn] * 5, entity_columns)
+        nimsp_urn_criteria = zip(urn_columns, [nimsp_urn] * 5, entity_columns)
+        
+        return (clean_name, 'organization', name_criteria + crp_urn_criteria + nimsp_urn_criteria)
+    
     for row_values in csv.reader(csv_rows):
+        print "Building entity for row '%s'" % row_values
         build_entity(*parse(*row_values))
+        
+        
 
 
 if __name__ == "__main__":
