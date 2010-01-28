462c278ad1abe0bd60557e4508b771b08ad21b7f
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2010-01-28 14:03:22 -0500

    Moved contribution aggregate queries to Python script. Changed entity contribution aggregate fields to allow nulls (which should be distinct from an aggregate of 0.) Changed search_entities_by_name to use stored aggregates. Added new tests for count and amount aggregates. Changed build_big_hitters to include contributor_employer field. Changed normalizer to include contributor_employer field. [#134] [#136]

--- dc_matchbox/matchbox/tests.py
+++ dc_matchbox/matchbox/tests.py
@@ -458,6 +458,28 @@ class TestEntityBuild(unittest.TestCase):
         self.assertEqual(2, Contribution.objects.filter(organization_entity=e.id).count())
         self.assertEqual(2, Contribution.objects.filter(parent_organization_entity=e.id).count())
         
+    def test_entity_aggregates(self):
+        self.create_contribution(contributor_name='FooBar', amount=100)
+        self.create_contribution(contributor_employer='FooBar Corp')
+        self.create_contribution(organization_name='ZapWow', amount=100)
+        self.create_contribution(parent_organization_name='ZapWow', amount=200)
+        self.create_contribution(committee_name='ZapWow', amount=400)
+        self.create_contribution(recipient_name='ZapWow', amount=800)
+        
+        whitelist = ["0, 0, FooBar", "0, 0, ZapWow"]
+        
+        normalize_contributions()
+        build_big_hitters(whitelist)
+        build_aggregates()
+        
+        foobar = Entity.objects.get(name="FooBar")
+        self.assertEqual(2, foobar.contribution_count)
+        self.assertEqual(100, foobar.contribution_amount)
+        
+        zapwow = Entity.objects.get(name="ZapWow")
+        self.assertEqual(4, zapwow.contribution_count)
+        self.assertEqual(1500, zapwow.contribution_amount)
+        
     
 class TestUtils(unittest.TestCase):
     def test_prepend_pluses(self):
--- dc_matchbox/matchbox_scripts/contribution/build_aggregates.py
+++ dc_matchbox/matchbox_scripts/contribution/build_aggregates.py
@@ -2,24 +2,79 @@
 from django.db import connection, transaction
 
 
+aggregate_count_stmt = """
+    update matchbox_entity set contribution_count = (
+        select count(*) from contribution_contribution 
+        where contributor_entity = matchbox_entity.id
+            or organization_entity = matchbox_entity.id
+            or parent_organization_entity = matchbox_entity.id
+            or committee_entity = matchbox_entity.id
+            or recipient_entity = matchbox_entity.id
+    )
+"""
+
+
+aggregate_amount_stmt = """
+    update matchbox_entity set contribution_amount = (
+        select sum(amount) from contribution_contribution
+        where contributor_entity = matchbox_entity.id
+            or organization_entity = matchbox_entity.id
+            or parent_organization_entity = matchbox_entity.id
+            or committee_entity = matchbox_entity.id
+            or recipient_entity = matchbox_entity.id
+    )
+"""
+
+build_aliases_stmt = """
+    insert into matchbox_entityalias (entity_id, alias)
+        select contributor_entity, contributor_name
+        from contribution_contribution
+        where contributor_entity != ''
+            and contributor_name != ''
+        group by contributor_entity, contributor_name
+    union
+        select organization_entity, organization_name
+        from contribution_contribution
+        where organization_entity != ''
+            and organization_name != ''
+        group by organization_entity, organization_name
+    union
+        select organization_entity, contributor_employer
+        from contribution_contribution
+        where organization_entity != ''
+            and contributor_employer != ''
+        group by organization_entity, contributor_employer
+    union
+        select parent_organization_entity, parent_organization_name
+        from contribution_contribution
+        where parent_organization_entity != ''
+            and parent_organization_name != ''
+        group by parent_organization_entity, parent_organization_name
+    union
+        select committee_entity, committee_name
+        from contribution_contribution
+        where committee_entity != ''
+            and committee_name != ''
+        group by committee_entity, committee_name
+    union
+        select recipient_entity, recipient_name
+        from contribution_contribution
+        where recipient_entity != ''
+            and recipient_name != ''
+        group by recipient_entity, recipient_name;
+"""
+
+# todo: modify alias query for attributes
+
+
+@transaction.commit_on_success
 def build_aggregates():
     cursor = connection.cursor()
+    transaction.set_dirty()
     
-    cursor.execute("drop table if exists matchbox_contribution_aggregates")
-    
-    cursor.execute("""
-                    create table matchbox_contribution_aggregates as
-                    select e.id, count(distinct c.id), sum(c.amount)
-                    from matchbox_entity e left join contribution_contribution c on
-                        e.id = c.contributor_entity or  
-                        e.id = c.organization_entity or
-                        e.id = c.parent_organization_entity or
-                        e.id = c.committee_entity or
-                        e.id = c.recipient_entity
-                    group by e.id """)
-
-    cursor.execute("create index matchbox_contribution_aggregates_id on matchbox_contribution_aggregates (id)")
-    
+    cursor.execute(aggregate_count_stmt)
+    cursor.execute(aggregate_amount_stmt)
+    cursor.execute(build_aliases_stmt)
     
 if __name__ == '__main__':
     build_aggregates()
