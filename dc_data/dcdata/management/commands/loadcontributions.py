b36a878c92f04e41cd5b86a7797dd5200e325dd1
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2010-02-04 11:37:17 -0500

    Removed code that generated default entity IDs. All entity fields now left blank on loading. [#162]

--- dc_data/dcdata/management/commands/loadcontributions.py
+++ dc_data/dcdata/management/commands/loadcontributions.py
@@ -17,54 +17,6 @@ import os
 import sys
 import traceback
 
-MATCHBOX_ORG_NAMESPACE = 'urn:matchbox:organization:'
-MATCHBOX_NAME_PREFIX = 'urn:matchbox:name:'
-MATCHBOX_COMMITTEE_NAME_PREFIX = 'urn:matchbox:committee_name:'
-
-
-#
-# entity filters
-#
-
-
-def get_with_name_fallback(preferred_field, fallback_field, urn_prefix):
-    def source(record):
-        if record.get(preferred_field, False):
-            return record[preferred_field]
-        name = record.get(fallback_field, None)
-        if name:
-            return urn_prefix + basic_normalizer(name.decode('utf-8', 'ignore'))
-        return None
-    
-    return source
-
-def get_with_contributor_fallback():
-    def source(record):
-        if record.get('contributor_urn', False):
-            return record['contributor_urn']
-        name = record.get('contributor_name', None)
-        if name:
-            city = record.get('contributor_city', None)
-            if not city:
-                city = 'unknown'
-            state = record.get('contributor_state', None)
-            if not state:
-                state = 'unknown'
-            (name, city, state) = (name.decode('utf-8', 'ignore'), city.decode('utf-8', 'ignore'), state.decode('utf-8', 'ignore'))
-            normalized_name = basic_normalizer(name)
-            return "%s%s, %s, %s" % (MATCHBOX_NAME_PREFIX, normalized_name, city, state)
-        return None
-    
-    return source    
-    
-
-
-CONTRIBUTOR_ENTITY_FILTER = MD5Filter(get_with_contributor_fallback(), 'contributor_entity')
-RECIPIENT_ENTITY_FILTER = MD5Filter(get_with_name_fallback('recipient_urn', 'recipient_name', MATCHBOX_NAME_PREFIX), 'recipient_entity')
-COMMITTEE_ENTITY_FILTER = MD5Filter(get_with_name_fallback('committee_urn', 'committee_name', MATCHBOX_COMMITTEE_NAME_PREFIX), 'committee_entity')
-ORGANIZATION_ENTITY_FILTER = MD5Filter(get_with_name_fallback('organization_urn', 'organization_name', MATCHBOX_NAME_PREFIX), 'organization_entity')
-PARENT_ORGANIZATION_ENTITY_FILTER = MD5Filter(get_with_name_fallback('parent_organization_urn', 'parent_organization_name', MATCHBOX_NAME_PREFIX), 'parent_organization_entity')
-
 
 class ContributorFilter(Filter):
     type_mapping = {'individual': 'I', 'committee': 'C', 'organization': 'O'}
@@ -197,12 +149,6 @@ class Command(BaseCommand):
                 ParentOrganizationFilter(),
                 RecipientFilter(),
                 CommitteeFilter(),
-                
-                CONTRIBUTOR_ENTITY_FILTER,
-                RECIPIENT_ENTITY_FILTER,
-                COMMITTEE_ENTITY_FILTER,
-                ORGANIZATION_ENTITY_FILTER,
-                PARENT_ORGANIZATION_ENTITY_FILTER,
                 
                 #DebugEmitter(),
                 AbortFilter(0.001), # fail if over 1 in 1000 records is bad
