671e66215b0ac3ef43be5d8c82086e4156f8cea2
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2010-02-16 16:19:11 -0500

    Fixed normalization index.

--- dc_matchbox/matchbox_scripts/matchbox_indexes.sql
+++ dc_matchbox/matchbox_scripts/matchbox_indexes.sql
@@ -1,7 +1,7 @@
 
 
 
-create index matchbox_normalization_normalized on matchbox_normalization (normalized);
+create index matchbox_normalization_normalized_like on matchbox_normalization (normalized varchar_pattern_ops);
 
 create index matchbox_normalization_original_ft on matchbox_normalization using gin(to_tsvector('datacommons', original));
 
