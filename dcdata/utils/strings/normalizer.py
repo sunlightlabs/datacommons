77f3e6b1062dd0a30a23ced40dafa7fd956a21ad
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-03-23 16:04:45 -0400

    fix paths in imports

--- dcdata/utils/strings/normalizer.py
+++ dcdata/utils/strings/normalizer.py
@@ -5,7 +5,7 @@ A collection of pre-configured normalizers.
 Only one demo normalizer for now.
 """
 
-from strings.transformers import compose, build_map_substrings, build_remove_substrings, build_remove_suffixes
+from dcdata.utils.strings.transformers import compose, build_map_substrings, build_remove_substrings, build_remove_suffixes
 
 
 basic_normalizer = compose(unicode.lower, 
