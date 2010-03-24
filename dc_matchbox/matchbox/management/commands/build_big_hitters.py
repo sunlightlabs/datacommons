ef66aadaf2627d9f08ffb69d95cf92493ab3fdd8
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2010-03-24 10:20:43 -0400

    Modifying for different input formats. In progress still...

--- dc_matchbox/matchbox/management/commands/build_big_hitters.py
+++ dc_matchbox/matchbox/management/commands/build_big_hitters.py
@@ -2,7 +2,8 @@
 import os
 
 from django.core.management.base import BaseCommand
-from matchbox.entity import build_org_entity
+from matchbox.entity import build_org_entity, build_recipient_entity
+from dcdata.contribution.models import CRP_TRANSACTION_NAMESPACE
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 import sys
@@ -12,10 +13,12 @@ import csv
 
 
 def build_big_hitters(csv_rows):
-    for (crp_id, nimsp_id, name) in csv.reader(csv_rows):
+    #for (crp_id, nimsp_id, name) in csv.reader(csv_rows):
+    for (name,id) in csv.reader(csv_rows):
         try:
             clean_name = name.strip().decode('utf8', 'replace')
-            build_org_entity(clean_name, crp_id.strip(), nimsp_id.strip())
+            #build_org_entity(clean_name, crp_id.strip(), nimsp_id.strip())
+            build_recipient_entity(clean_name, CRP_TRANSACTION_NAMESPACE, id)
         except:
             traceback.print_exception(*sys.exc_info())
             print "!!!!! Skipping Entity: %s !!!!!" % name
