8a4d8f36bfee36d5500cb2ddda196ccb62d93a5c
jessykate <jessy.cowansharp@gmail.com>
2010-03-25 12:26:00 -0400

    added comments showing future dict return structure to entity search

--- dcapi/entities/handlers.py
+++ dcapi/entities/handlers.py
@@ -4,8 +4,8 @@ import sys
 from urllib import unquote_plus
 from django.db.models import Q
 from piston.handler import BaseHandler
-from matchbox.models import Entity
-from matchbox.queries import search_entities_by_name
+from dcentity.models import Entity
+from dcentity.queries import search_entities_by_name
 
 class EntityHandler(BaseHandler):
     allowed_methods = ('GET',)
@@ -28,9 +28,25 @@ class EntityFilterHandler(BaseHandler):
         if not search_string:
             return {'response' : "It doesn't make any sense to do a search without a search string"}
 
-        entity_type = request.GET.get('type', None)
-        if 'search' in request.GET:
-            return search_entities_by_name(unicode(request.GET['search']), entity_type)
+        entity_types = request.GET.get('type', None)
+        # search_entities_by_name expects a list for the type filter,
+        # even if there's only one element.
+        if entity_types:
+            entity_types = [entity_type.strip() for entity_type in entity_types.split(',')]
+        return search_entities_by_name(search_string, entity_types)
+
+
+#        results = []
+#        for (id_, name, count, total_given, total_received) in search_entities_by_name(search_string, entity_types):
+#            results.append({
+#                    'id': id_,
+#                    'type': 'organization',
+#                    'name': name,
+#                    'count': count,
+#                    'total_given': float(total_given),
+#                    'total_received': float(total_received)
+#                    })
+#        return results
         
 
 
