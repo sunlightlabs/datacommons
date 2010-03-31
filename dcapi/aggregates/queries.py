6becd922a755619c21c2d2932cf04a659b2094e7
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2010-03-31 16:27:54 -0400

    Working on new lightweight entity creation approach.

--- dcapi/aggregates/queries.py
+++ dcapi/aggregates/queries.py
@@ -3,12 +3,8 @@ from django.db import connection
 
 create_top_stmt = """
     create view agg_contributions as
-        select case when contributor_entity = '' then contributor_name
-                    else contributor_entity
-               end as contributor,
-               case when recipient_entity = '' then recipient_name
-                    else recipient_entity
-               end as recipient,
+        select coalesce(c.entity_id, t.contributor_name) as contributor,
+               coalesce(r.entity_id, t.recipient_name) as recipient,
                contributor_type,
                recipient_type,
                transaction_type,
@@ -17,11 +13,13 @@ create_top_stmt = """
                cycle,
                count(*) as count,
                sum(amount) as amount
-        from contribution_contribution
-        group by case when contributor_entity = '' then contributor_name
-                        else contributor_entity end,
-                 case when recipient_entity = '' then recipient_name
-                         else recipient_entity end,
+        from contribution_contribution t
+        left outer join contributor_associations c
+            on c.transaction_id = t.transaction_id
+        left outer join recipient_associations r
+            on r.transaction_id = t.transaction_id
+        group by coalesce(c.entity_id, t.contributor_name),
+                 coalesce(r.entity_id, t.recipient_name),
                  contributor_type,
                  recipient_type,
                  transaction_type,
@@ -159,22 +157,34 @@ get_top_cmtes_from_indiv_stmt = """
 #
 
 search_stmt = """
-    (select coalesce(c.name, r.name) as name, '' as entity_id, coalesce(c.count, 0) + coalesce(r.count, 0) as count, coalesce(c.given, 0) as given, coalesce(r.received, 0) as received
+    (select coalesce(c.name, r.name) as name, '' as entity_id, coalesce(c.count, 0) as count_given, coalesce(r.count, 0) as count_received, coalesce(c.given, 0) as given, coalesce(r.received, 0) as received
     from
         (select contributor as name, '' as entity_id, sum(count) as count, sum(amount) as given, 0 as received
         from agg_contributions
         where to_tsvector('datacommons', contributor) @@ to_tsquery('datacommons', %s)
-        group by contributor) as c    
+        group by contributor
+        having sum(count) > 0) as c    
     full join    
         (select recipient as name, '' as entity_id, sum(count) as count, 0 as given, sum(amount) as received
         from agg_contributions
         where to_tsvector('datacommons', recipient) @@ to_tsquery('datacommons', %s)
-        group by recipient) as r
+        group by recipient
+        having sum(count) > 0) as r
     on c.name = r.name)
 union
-    (select e.name, e.id, e.contribution_count, e.contribution_total_given, e.contribution_total_received
+    (select e.name, e.id, count(c), count(r), sum(ct.amount), sum(rt.amount)
     from matchbox_entity e
-    where to_tsvector('datacommons', name) @@ to_tsquery('datacommons', %s))
+    left outer join contributor_associations c
+        on e.id = c.entity_id
+    left outer join contribution_contribution ct
+        on c.transaction_id = ct.transaction_id
+    left outer join recipient_associations r
+        on e.id = r.entity_id
+    left outer join contribution_contribution rt
+        on r.transaction_id = rt.transaction_id
+    where to_tsvector('datacommons', name) @@ to_tsquery('datacommons', %s)
+    group by e.name, e.id
+    having count(c) > 0 or count(r) > 0)
 """
 
 
@@ -187,18 +197,21 @@ def search_names(query, entity_types):
     parsed_query = ' & '.join(query.split(' '))
     
     cursor.execute(search_stmt, [parsed_query, parsed_query, parsed_query])
-    results = list(cursor)
-    results_annotated = []
-    for (name, id_, count, total_given, total_received) in results:
-        results_annotated.append({
-                'id': id_,
-                'name': name,
-                'count': count,
-                'total_given': float(total_given),
-                'total_received': float(total_received)
-                })
-    return results_annotated
-   
+    return list(cursor)
+    
+#    
+#    results_annotated = []
+#    for (name, id_, count_given, count_received, total_given, total_received) in results:
+#        results_annotated.append({
+#                'id': id_,
+#                'name': name,
+#                'count_given': count_given,
+#                'count_received': count_received,
+#                'total_given': float(total_given),
+#                'total_received': float(total_received)
+#                })
+#    return results_annotated
+#   
 
 
 def _cycle_clause(cycles):
