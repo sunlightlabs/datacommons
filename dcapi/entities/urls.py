7e5c6f577a44c38c79156d8c896547585ce1268d
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2010-05-04 13:55:10 -0400

    Added handler and URLs for getting entity contribution totals.

--- dcapi/entities/urls.py
+++ dcapi/entities/urls.py
@@ -1,5 +1,5 @@
 from dcapi.entities.handlers import EntityHandler, EntityFilterHandler, \
-    EntityAttributeHandler
+    EntityAttributeHandler, EntityTotalsHandler
 from django.conf.urls.defaults import *
 from locksmith.auth.authentication import PistonKeyAuthentication
 from piston.emitters import Emitter
@@ -20,9 +20,11 @@ ad = { 'authentication': PistonKeyAuthentication() }
 entity_handler = Resource(EntityHandler, **ad)
 entityfilter_handler = Resource(EntityFilterHandler, **ad)
 entity_attribute_handler = Resource(EntityAttributeHandler, **ad)
+entity_totals_handler = Resource(EntityTotalsHandler, **ad)
 
 urlpatterns = patterns('',
     url(r'^/id_lookup$', entity_attribute_handler, name='api_entity_attribute'),
+    url(r'^/(?P<entity_id>\w+)/totals.(?P<emitter_format>.+)$', entity_totals_handler, name='entity_totals_handler'),
     url(r'^/(?P<entity_id>\w+).(?P<emitter_format>.+)$', entity_handler, name='api_entities'),
     url(r'^.(?P<emitter_format>.+)$', entityfilter_handler, name='api_entities_filter'),
 )
