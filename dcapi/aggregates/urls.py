9e65af5ad9a2764a769fa10457180ece149a5643
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2010-05-06 20:49:43 -0400

    Inlined handlers.

--- dcapi/aggregates/urls.py
+++ dcapi/aggregates/urls.py
@@ -21,75 +21,58 @@ Emitter.unregister('yaml')
 ad = { 'authentication': PistonKeyAuthentication() }
 
 
-org_recipients_handler = Resource(OrgRecipientsHandler, **ad)
-pol_contributors_handler = Resource(PolContributorsHandler, **ad)
-indiv_org_recipients_handler = Resource(IndivOrgRecipientsHandler, **ad)
-indiv_pol_recipients_handler = Resource(IndivPolRecipientsHandler, **ad)
-sectors_handler = Resource(SectorsHandler, **ad)
-industries_by_sector_handler = Resource(IndustriesBySectorHandler, **ad)
-org_level_breakdown_handler = Resource(OrgLevelBreakdownHandler, **ad)
-org_party_breakdown_handler = Resource(OrgPartyBreakdownHandler, **ad)
-indiv_party_breakdown_handler = Resource(IndivPartyBreakdownHandler, **ad)
-pol_local_breakdown_handler = Resource(PolLocalBreakdownHandler, **ad)
-pol_type_breakdown_handler = Resource(PolContributorTypeBreakdownHandler, **ad)
-
-lobbying_org_registrants_handler = Resource(OrgRegistrantsHandler, **ad)
-lobbying_org_issues_handler = Resource(OrgIssuesHandler, **ad)
-lobbying_org_lobbyists_handler = Resource(OrgLobbyistsHandler, **ad)
-
-
 urlpatterns = patterns('',
 
     # contributors to a single politician 
     url(r'^pol/(?P<entity_id>.+)/contributors\.(?P<emitter_format>.+)$', 
-        pol_contributors_handler),
+        Resource(PolContributorsHandler, **ad)),
 
     # contributions to a single politician, broken down by industry
     url(r'^pol/(?P<entity_id>.+)/contributors/sectors\.(?P<emitter_format>.+)$', 
-        sectors_handler),
+        Resource(SectorsHandler, **ad)),
 
     # contributions to a single politician, broken down by industry sector
     url(r'^pol/(?P<entity_id>.+)/contributors/sector/(?P<sector>.+)/industries\.(?P<emitter_format>.+)$', 
-        industries_by_sector_handler),
+        Resource(IndustriesBySectorHandler, **ad)),
 
     # contributions to a single politician, broken down to show percentages
     url(r'^pol/(?P<entity_id>.+)/contributors/local_breakdown\.(?P<emitter_format>.+)$', 
-        pol_local_breakdown_handler),
+        Resource(PolLocalBreakdownHandler, **ad)),
     
     url(r'^pol/(?P<entity_id>.+)/contributors/type_breakdown\.(?P<emitter_format>.+)$', 
-        pol_type_breakdown_handler),
+        Resource(PolContributorTypeBreakdownHandler, **ad)),
     
     # recipients from a single individual, broken down to show percentages
     url(r'^indiv/(?P<entity_id>.+)/recipients/party_breakdown\.(?P<emitter_format>.+)$', 
-        indiv_party_breakdown_handler),
+        Resource(IndivPartyBreakdownHandler, **ad)),
 
     # recipients from a single individual
     url(r'^indiv/(?P<entity_id>.+)/recipient_orgs\.(?P<emitter_format>.+)$', 
-        indiv_org_recipients_handler),
+        Resource(IndivOrgRecipientsHandler, **ad)),
 
     url(r'^indiv/(?P<entity_id>.+)/recipient_pols\.(?P<emitter_format>.+)$', 
-        indiv_pol_recipients_handler),
+        Resource(IndivPolRecipientsHandler, **ad)),
 
 
     # recipients from a single org//pac 
     url(r'^org/(?P<entity_id>.+)/recipients\.(?P<emitter_format>.+)$', 
-        org_recipients_handler),
+        Resource(OrgRecipientsHandler, **ad)),
 
     # recipients from a single org, broken down to show percentages
     url(r'^org/(?P<entity_id>.+)/recipients/party_breakdown\.(?P<emitter_format>.+)$', 
-        org_party_breakdown_handler),
+        Resource(OrgPartyBreakdownHandler, **ad)),
 
     url(r'^org/(?P<entity_id>.+)/recipients/level_breakdown\.(?P<emitter_format>.+)$', 
-        org_level_breakdown_handler),
+        Resource(OrgLevelBreakdownHandler, **ad)),
 
     url(r'^org/(?P<entity_id>.+)/registrants\.(?P<emitter_format>.+)$',
-        lobbying_org_registrants_handler),
+        Resource(OrgRegistrantsHandler, **ad)),
         
     url(r'^org/(?P<entity_id>.+)/issues\.(?P<emitter_format>.+)',
-        lobbying_org_issues_handler),
+        Resource(OrgIssuesHandler, **ad)),
         
     url(r'org/(?P<entity_id>.+)/lobbyists\.(?P<emitter_format>.+)',
-        lobbying_org_lobbyists_handler),
+        Resource(OrgLobbyistsHandler, **ad)),
 )
 
 
