6e3892fa3d18bed830d644623c5b9747612deb7c
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2010-05-14 17:15:41 -0400

    Utility scripts for generating entities.

---
+++ dcentity/management/entity_sources.sql
@@ -0,0 +1,27 @@
+
+
+
+-- Individual Entities
+
+create table tmp_individuals as
+    select min(lobbyist_name) as name, lobbyist_ext_id as id
+    from lobbying_lobbyist
+    group by lobbyist_ext_id
+union
+    select min(contributor_name) as name, contributor_ext_id as id
+    from contribution_contribution
+    where
+        contributor_ext_id like 'U%'
+        and not exists (select * from lobbying_lobbyist where lobbyist_ext_id = contributor_ext_id)
+    group by contributor_ext_id;
+    
+
+
+-- Politician Entities
+
+create table tmp_politicians as
+    select min(recipient_name) as name, transaction_namespace as namespace, recipient_ext_id as id
+    from contribution_contribution
+    where
+        recipient_type = 'P'
+    group by transaction_namespace, recipient_ext_id;
