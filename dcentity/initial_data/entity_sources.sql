68fd8352d08154fed08c6b4cb4e33977457ffcd1
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2010-05-21 17:45:08 -0400

    Ignore empty names.

--- dcentity/initial_data/entity_sources.sql
+++ dcentity/initial_data/entity_sources.sql
@@ -6,12 +6,15 @@
 create table tmp_individuals as
     select min(lobbyist_name) as name, lobbyist_ext_id as id
     from lobbying_lobbyist
+    where
+        lobbyist_name != ''
     group by lobbyist_ext_id
 union
     select min(contributor_name) as name, contributor_ext_id as id
     from contribution_contribution
     where
-        contributor_ext_id like 'U%'
+        contributor_name != ''
+        and contributor_ext_id like 'U%'
         and not exists (select * from lobbying_lobbyist where lobbyist_ext_id = contributor_ext_id)
     group by contributor_ext_id;
     
@@ -24,4 +27,5 @@ create table tmp_politicians as
     from contribution_contribution
     where
         recipient_type = 'P'
+        and recipient_name != ''
     group by transaction_namespace, recipient_ext_id;
