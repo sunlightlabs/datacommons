5e4c42119e66681ee05142154ecb43844c72bf88
Andrew Pendleton <apendleton@sunlightfoundation.com>
2010-09-10 19:08:12 -0400

    A bunch of fixes and improvements. Now supports graphs, industries, and aggregates.

--- dcapi/pg_rapportive/templates/pg_rapportive/poligraft.js
+++ dcapi/pg_rapportive/templates/pg_rapportive/poligraft.js
@@ -38,6 +38,7 @@
         this.state = 'not fetched';
         this.hash = hash;
         this.index = index;
+        this.remapped = false;
     }
     
     PgMessage.prototype.getDiv = function() {
@@ -65,7 +66,6 @@
                         if (realData.processed) {
                             origMessage.pgData = realData;
                             origMessage.state = 'fetched';
-                            console.log(realData);
                             clearInterval(interval);
                             callback();
                         }
@@ -82,17 +82,24 @@
                 div.removeClass('pg-fetching');
                 div.addClass('pg-rendered');
                 
+                this.remapIfNecessary();
+                
                 var text = div.html();
                 $.each(this.pgData.entities, function(num, entity) {
                     if (!entity.tdata_id) return;
                     var label = "<span class='pg-wrapper'><div class='pg-insert'>"
-                        + "<h3>" + entity.tdata_name +"</h3>"
-                        + (entity.recipient_breakdown.dem === undefined ? "" : "Demsocrats: " + entity.recipient_breakdown.dem + "%; Republicans: " + entity.recipient_breakdown.rep + "%")
-                        + (entity.contributor_breakdown.individual === undefined ? "" : "Individual: " + entity.contributor_breakdown.individual + "%; Republicans: " + entity.contributor_breakdown.pac + "%")
+                        + "<a href='http://influenceexplorer.com/" + entity.tdata_type + "/" + entity.tdata_slug + "/" + entity.tdata_id + "'><h3>" + entity.tdata_name +"</h3></a>"
+                        + (entity.recipient_breakdown.dem === undefined ? "" : "<img src='http://chart.apis.google.com/chart?cht=p&amp;chf=bg,s,FFFFFF&amp;chp=1.57&amp;chs=145x50&amp;chco=3072F3|DB2A3F&amp;chd=t:" + entity.recipient_breakdown.dem + "," + entity.recipient_breakdown.rep + "&amp;chdl=Democrats|Republicans' alt='Chart for " + entity.name + "'>")
+                        + (entity.contributor_breakdown.individual === undefined ? "" : "<img src='http://chart.apis.google.com/chart?cht=p&amp;chf=bg,s,FFFFFF&amp;chp=1.57&amp;chs=145x50&amp;chco=ABDEBF|169552&amp;chd=t:" + entity.contributor_breakdown.individual + "," + entity.contributor_breakdown.pac + "&amp;chdl=Individuals|PACs' alt='Chart for " + entity.name + "'>")
+                        + (entity.top_industries.length == 0 ? "" : "<div class='pg-info'><strong>Top Contributing Industries:</strong><ul><li>" + entity.top_industries.join("</li><li>") + "</li></div>")
+                        + (entity.contributors.length == 0 ? "" : "<div class='pg-info'><strong>Relevant Contributions:</strong><ul><li>" + $.map(entity.contributors, function(cont) { return "<strong>$" + cont.amount + "</strong> " + (entity.tdata_type == 'politician' ? 'from' : 'to') + " <strong><a href='http://influenceexplorer.com/" + cont.tdata_type + "/" + cont.tdata_slug + "/" + cont.tdata_id + "'>" + cont.tdata_name + "</a></strong>."; }).join("</li><li>") + "</li></ul></div>")
+                        + "<br /><a href='http://influenceexplorer.com/" + entity.tdata_type + "/" + entity.tdata_slug + "/" + entity.tdata_id + "'>Learn More &raquo;</a>"
                         + "</div><span class='pg-highlighted'>" + entity.name + "</span></span>";
-                    text = text.replace(entity.name, label);
+                    text = text.replace(new RegExp(entity.name, 'g'), label);
                 })
                 div.html(text);
+                
+                div.find('.pg-wrapper .pg-wrapper').removeClass('.pg-wrapper').find('.pg-insert').remove();
                 
                 div.find('.pg-wrapper').hover(function() {
                     $(this).find('.pg-insert').show();
@@ -113,6 +120,20 @@
         origMessage.renderIfAvailable();
     }
     
+    PgMessage.prototype.remapIfNecessary = function() {
+        if (this.remapped) return;
+        var message = this;
+        $.each(this.pgData.entities, function(num, entity) {
+            if (entity.contributors.length != 0 && entity.tdata_type == 'politician') {
+                $.each(entity.contributors, function(cnum, contributor) {
+                    var origContributor = $.map(message.pgData.entities, function(element) { return element.tdata_id == contributor.tdata_id ? element : null; })[0];
+                    origContributor.contributors.push({tdata_name: entity.tdata_name, tdata_id: entity.tdata_id, tdata_type: entity.tdata_type, tdata_slug: entity.tdata_slug, amount: contributor.amount})
+                });
+            }
+        });
+        this.remapped = true;
+    }
+    
     
     // Check to see if the Poligraft class has already been loaded and, if not, load it
     if (window.poligraftParser === undefined) {
@@ -128,7 +149,6 @@
         }
     })
     if (!stylesLoaded) {
-        console.log('loading the sheet');
         $(document.documentElement).find('head').append('<link rel="stylesheet" type="text/css" href="{{ host }}/media/css/poligraft-rapportive.css" />')
     }
     
