42f5976d2228ae2c592d7f877d2ebcf4ee93ca66
Andrew Pendleton <apendleton@sunlightfoundation.com>
2010-09-27 14:35:16 -0400

    Refactoring management command for S3 uploads; refs #527

--- dcdata/management/commands/uploadbulk.py
+++ dcdata/management/commands/uploadbulk.py
@@ -10,26 +10,36 @@ except ImportError:
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 
+try:
+    import boto
+    from boto.s3.key import Key
+except ImportError:
+    raise ImproperlyConfigured, "Could not load Boto's S3 bindings. See http://code.google.com/p/boto/"
+
 def progress(complete, total):
     sys.stdout.write('Uploaded %s bytes of %s (%s%%)\n' % (complete, total, int(100*complete/total)))
 
 class Command(BaseCommand):
-    args = "<filename> [<upload_filename>]"
+    args = "<filename>"
     help = "Uploads a bulk file to S3."
     option_list = BaseCommand.option_list + (
         make_option('-z', '--gzip', action='store_true', dest='gzip', default=False, help='Compress the file using gzip (caution: compressed file will be stored in-memory)'),
     )
     
-    def handle(self, filename, upload_filename='', *args, **options):
-        try:
-            import boto
-            from boto.s3.key import Key
-        except ImportError:
-            raise ImproperlyConfigured, "Could not load Boto's S3 bindings. See http://code.google.com/p/boto/"
-        
+    def handle(self, *files, **options):
         conf = getattr(settings, 'BULKDATA', None)
         if not conf:
             raise ImproperlyConfigured("No BULKDATA setting found.")
+        
+        sys.stdout.write('Connecting to S3...\n')
+        self.conn = boto.connect_s3(conf['AWS_KEY'], conf['AWS_SECRET'])
+        self.bucket = self.conn.get_bucket(conf['AWS_BUCKET'])
+        
+        for file in files:
+            self.upload_file(file, options)
+    
+    def upload_file(self, filename, options):
+        conf = getattr(settings, 'BULKDATA', None)
         
         url = conf['AWS_PREFIX']
         try:
@@ -37,10 +47,13 @@ class Command(BaseCommand):
         except:
             pass
 
-        if not upload_filename:
-            upload_filename = os.path.basename(filename)
+        
+        upload_filename = os.path.basename(filename)
         
         url = os.path.join(url, upload_filename)
+        
+        if options['gzip']:
+            url = url + '.gz'
         
         file = open(filename)
         
@@ -57,10 +70,7 @@ class Command(BaseCommand):
         else:
             upload = file
         
-        sys.stdout.write('Connecting to S3...\n')
-        conn = boto.connect_s3(conf['AWS_KEY'], conf['AWS_SECRET'])
-        bucket = conn.get_bucket(conf['AWS_BUCKET'])
-        k = Key(bucket)
+        k = Key(self.bucket)
         k.key = url
         
         sys.stdout.write('Uploading...\n')
