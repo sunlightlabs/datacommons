bcf8d3184c6182a9060c8b95658935c27a20e833
Alison Rowland <arowland@sunlightfoundation.com>
2010-10-12 22:15:43 -0400

    Making entity metadata script be able to run in the background.

--- dcapi/aggregates/entities/entity_metadata.sql
+++ dcapi/aggregates/entities/entity_metadata.sql
@@ -3,9 +3,10 @@
 
 -- Organization Metadata
 
-delete from matchbox_organizationmetadata;
+begin;
+create temp table tmp_matchbox_organizationmetadata as select * from matchbox_organizationmetadata limit 0;
 
-insert into matchbox_organizationmetadata (entity_id, lobbying_firm, parent_entity_id, industry_entity_id)
+insert into tmp_matchbox_organizationmetadata (entity_id, lobbying_firm, parent_entity_id, industry_entity_id)
     select
         entity_id,
         bool_or(coalesce(lobbying_firm, 'f')) as lobbying_firm,
@@ -37,12 +38,19 @@ insert into matchbox_organizationmetadata (entity_id, lobbying_firm, parent_enti
     ) contributing_orgs using (entity_id)
     group by entity_id;
 
+delete from matchbox_organizationmetadata;
+
+insert into matchbox_organizationmetadata (entity_id, lobbying_firm, parent_entity_id, industry_entity_id)
+    select entity_id, lobbying_firm, parent_entity_id, industry_entity_id from tmp_matchbox_organizationmetadata;
+commit;
+
 
 -- Politician Metadata
 
-delete from matchbox_politicianmetadata;
+begin;
+create temp table tmp_matchbox_politicianmetadata as select * from matchbox_politicianmetadata limit 0;
 
-insert into matchbox_politicianmetadata (entity_id, state, party, seat)
+insert into tmp_matchbox_politicianmetadata (entity_id, state, party, seat)
     select distinct on (entity_id) entity_id, recipient_state, recipient_party, seat
     from contribution_contribution c
     inner join recipient_associations ra using (transaction_id)
@@ -51,12 +59,19 @@ insert into matchbox_politicianmetadata (entity_id, state, party, seat)
         e.type = 'politician'
     order by entity_id, cycle desc;
 
+delete from matchbox_politicianmetadata;
+
+insert into matchbox_politicianmetadata (entity_id, state, party, seat)
+    select entity_id, state, party, seat from tmp_matchbox_politicianmetadata;
+commit;
+
 
 -- Indiv/Org Affiliations
 
-delete from matchbox_indivorgaffiliations;
+begin;
+create temp table tmp_matchbox_indivorgaffiliations as select * from matchbox_indivorgaffiliations limit 0;
 
-insert into matchbox_indivorgaffiliations (individual_entity_id, organization_entity_id)
+insert into tmp_matchbox_indivorgaffiliations (individual_entity_id, organization_entity_id)
     select distinct
         ca.entity_id as individual_entity_id, oa.entity_id as organization_entity_id
     from
@@ -64,6 +79,12 @@ insert into matchbox_indivorgaffiliations (individual_entity_id, organization_en
         inner join organization_associations oa using (transaction_id)
         inner join matchbox_entity me on me.id = ca.entity_id
     where me.type = 'individual';
+
+delete from matchbox_indivorgaffiliations;
+
+insert into matchbox_indivorgaffiliations (individual_entity_id, organization_entity_id)
+    select individual_entity_id, organization_entity_id from tmp_matchbox_indivorgaffiliations;
+commit;
 
 
 
