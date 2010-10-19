1a4dba54f842fcdd995dd57df6e24eb528784839
Alison Rowland <arowland@sunlightfoundation.com>
2010-10-19 13:33:16 -0400

    Remove *_entity indexes from drop_contribution_indexes.sql

--- dcdata/scripts/drop_contribution_indexes.sql
+++ dcdata/scripts/drop_contribution_indexes.sql
@@ -1,9 +1,5 @@
 drop index if exists contribution_contribution_contributor_category;
 drop index if exists contribution_contribution_contributor_category_order;
-drop index if exists contribution_contribution_contributor_entity;
-drop index if exists contribution_contribution_organization_entity;
-drop index if exists contribution_contribution_parent_organization_entity;
-drop index if exists contribution_contribution_recipient_entity;
 drop index if exists contribution_contribution_contributor_ext_id;
 drop index if exists contribution_contribution_organization_ext_id;
 drop index if exists contribution_contribution_parent_organization_ext_id;
