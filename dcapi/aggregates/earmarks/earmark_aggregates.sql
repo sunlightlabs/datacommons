afe321507fb6848db7710b5e8e4b359142483da6
Alison Rowland <arowland@sunlightfoundation.com>
2010-12-01 13:15:04 -0500

    [#678] Added table agg_earmark_totals.

--- dcapi/aggregates/earmarks/earmark_aggregates.sql
+++ dcapi/aggregates/earmarks/earmark_aggregates.sql
@@ -43,4 +43,40 @@ create index earmarks_recipient_associations_entity_id on earmarks_recipient_ass
 select date_trunc('second', now()) || ' -- create index earmarks_recipient_associations_earmark_id on earmarks_recipient_associations (earmark_id)';
 create index earmarks_recipient_associations_earmark_id on earmarks_recipient_associations (earmark_id);
 
+-- Earmark Totals
 
+select date_trunc('second', now()) || ' -- drop table if exists agg_earmark_totals';
+drop table if exists agg_earmark_totals;
+
+select date_trunc('second', now()) || ' -- create table agg_earmark_totals';
+create table agg_earmark_totals as
+    with earmarks_by_entity_cycle as (
+        select
+            coalesce(ma.entity_id, ra.entity_id) as entity_id,
+            case when fiscal_year % 2 = 0 then fiscal_year else fiscal_year + 1 end as cycle,
+            count(*) as count,
+            sum(final_amount) as amount
+        from
+            earmarks_earmark e
+            left join earmarks_member_associations ma on e.id = ma.earmark_id
+            left join earmarks_recipient_associations ra on e.id = ra.earmark_id
+        group by
+            coalesce(ma.entity_id, ra.entity_id),
+            case when fiscal_year % 2 = 0 then fiscal_year else fiscal_year + 1 end
+    )
+    select entity_id, cycle, count, amount
+    from earmarks_by_entity_cycle
+
+    union all
+
+    select entity_id, -1, sum(count) as count, sum(amount) as amount
+    from earmarks_by_entity_cycle
+    group by entity_id
+;
+
+select date_trunc('second', now()) || ' -- create index agg_earmark_totals_entity_id on agg_earmark_totals (entity_id)';
+create index agg_earmark_totals_entity_id on agg_earmark_totals (entity_id);
+select date_trunc('second', now()) || ' -- create index agg_earmark_totals_cycle on agg_earmark_totals (cycle)';
+create index agg_earmark_totals_cycle on agg_earmark_totals (cycle);
+
+select date_trunc('second', now()) || ' -- Finished computing earmark aggregates.';
