50406d9c783b9996b0e47d7accf064995ef1e48c
Alison Rowland <arowland@sunlightfoundation.com>
2010-12-20 11:54:05 -0500

    [#711] Added SQL update file to put new fields on contribution table related to seat status

---
+++ sql_updates/711_add_new_seat_info_fields_to_contribution.sql
@@ -0,0 +1,3 @@
+alter table contribution_contribution add column recipient_state_held varchar(2);
+alter table contribution_contribution add column district_held varchar(8);
+alter table contribution_contribution add column seat_held varchar(64);
