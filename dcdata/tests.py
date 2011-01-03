6d2203e3ac1230cddfbc4b29301681bbccfe0cc8
Alison Rowland <arowland@sunlightfoundation.com>
2011-01-03 15:43:22 -0500

    [#722] Fixing a bug in NIMSPDenormalize and removing some SkipTest statements for it

--- dcdata/tests.py
+++ dcdata/tests.py
@@ -32,7 +32,7 @@ import shutil
 import sqlite3
 import sys
 
-#from dcdata.management.commands.nimsp_denormalize import NIMSPDenormalize
+from dcdata.management.commands.nimsp_denormalize import NIMSPDenormalize
 
 
 dataroot = os.path.abspath(os.path.join(os.path.dirname(__file__), 'test_data'))
@@ -113,7 +113,7 @@ class TestNIMSPDenormalize(TestCase):
 
 
     def test_salting(self):
-        raise SkipTest
+        #raise SkipTest
         input_string = '"3327568","341.66","2006-11-07","MISC CONTRIBUTIONS $10000 AND UNDER","UNITEMIZED DONATIONS",\
                         "MISC CONTRIBUTIONS $100.00 AND UNDER","","","","","","","","","","","OR","","Z2400","0","0",\
                         "0",\N,"0","1825","PAC 483","2006",\N,\N,\N,\N,\N,\N,"I","PAC 483","130","OR"'
@@ -124,17 +124,15 @@ class TestNIMSPDenormalize(TestCase):
 
         load_data(source, processor, output.append)
 
-
         self.assertEqual(2, len(output))
         self.assertAlmostEqual(Decimal('341.66'), output[0]['amount'] + output[1]['amount'])
 
 
     def test_output_switch(self):
-        raise SkipTest
         self.assertFalse(os.path.exists(self.output_paths[0]))
         self.assertFalse(os.path.exists(self.output_paths[1]))
 
-        call_command('nimsp_denormalize', dataroot=dataroot, saltsdb=self.salts_db_path)
+        call_command('nimsp_denormalize', dataroot=dataroot, saltsdb=self.salts_db_path, dest_dir=os.path.join(dataroot, 'denormalized'))
 
         self.assertTrue(os.path.exists(self.output_paths[0]))
         self.assertTrue(os.path.exists(self.output_paths[1]))
@@ -142,7 +140,7 @@ class TestNIMSPDenormalize(TestCase):
         os.remove(self.output_paths[1])
         self.assertFalse(os.path.exists(self.output_paths[1]))
 
-        call_command('nimsp_denormalize', dataroot=dataroot, saltsdb=self.salts_db_path, output_types='unallocated')
+        call_command('nimsp_denormalize', dataroot=dataroot, saltsdb=self.salts_db_path, output_types='unallocated', dest_dir=os.path.join(dataroot, 'denormalized'))
 
         self.assertTrue(os.path.exists(self.output_paths[1]))
 
@@ -151,16 +149,16 @@ class TestNIMSPDenormalize(TestCase):
         self.assertFalse(os.path.exists(self.output_paths[0]))
         self.assertFalse(os.path.exists(self.output_paths[1]))
 
-        call_command('nimsp_denormalize', dataroot=dataroot, saltsdb=self.salts_db_path, output_types='allocated')
+        call_command('nimsp_denormalize', dataroot=dataroot, saltsdb=self.salts_db_path, output_types='allocated', dest_dir=os.path.join(dataroot, 'denormalized'))
 
         self.assertTrue(os.path.exists(self.output_paths[0]))
         self.assertFalse(os.path.exists(self.output_paths[1]))
 
 
     def test_command(self):
-        raise SkipTest
+        #raise SkipTest
 
-        call_command('nimsp_denormalize', dataroot=dataroot, saltsdb=self.salts_db_path)
+        call_command('nimsp_denormalize', dataroot=dataroot, saltsdb=self.salts_db_path, dest_dir=os.path.join(dataroot, 'denormalized'))
 
         self.assertEqual(9, sum(1 for _ in open(self.output_paths[0], 'r')))
         self.assertEqual(4, sum(1 for _ in open(self.output_paths[1], 'r')))
@@ -174,15 +172,13 @@ class TestNIMSPDenormalize(TestCase):
 
 
     def test_recipient_state(self):
-        raise SkipTest
+        #raise SkipTest
         self.test_command()
 
         self.assertEqual(7, Contribution.objects.filter(recipient_state='OR').count())
         self.assertEqual(2, Contribution.objects.filter(recipient_state='WA').count())
 
     def test_salt_filter(self):
-        raise SkipTest
-
         connection = sqlite3.connect(self.salts_db_path)
         connection.cursor().execute('delete from salts where nimsp_id = 9999')
         connection.commit()
@@ -200,7 +196,6 @@ class TestNIMSPDenormalize(TestCase):
         self.assertEqual(2, len(output))
 
     def test_contributor_type(self):
-        raise SkipTest
         input_string = '"3327568","341.66","2006-11-07","Adams, Kent","Adams, Kent",\
                         "MISC CONTRIBUTIONS $100.00 AND UNDER","","","","Adams & Boswell","","","","","","","OR","","A1000","0","0",\
                         "0",\N,"0","1825","PAC 483","2006",\N,\N,\N,\N,\N,\N,"I","PAC 483","130","OR"'
