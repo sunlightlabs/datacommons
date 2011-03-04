3187d58d1dddacff4e52912726c85c5ccee631f7
Alison Rowland <arowland@sunlightfoundation.com>
2011-03-03 19:18:13 -0500

    [#767] Updating Bioguide override script to reflect new copy date (for newly gotten Bioguide info)

--- sql_updates/767_copy_bioguide_photo_url_to_sunlight.sql
+++ sql_updates/767_copy_bioguide_photo_url_to_sunlight.sql
@@ -1,4 +1,4 @@
 insert into matchbox_sunlightinfo (entity_id, photo_url, notes)
-    select entity_id, photo_url, '3/1/2011: Copying Bioguide photo_url here to override newly scraped politician photos from Wikipedia.'
+    select entity_id, photo_url, '3/3/2011: Copying Bioguide photo_url here to override newly scraped politician photos from Wikipedia.'
 from matchbox_bioguideinfo bioguide
 where bioguide.photo_url is not null;
