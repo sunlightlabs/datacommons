e7281191c579df822744ddd442930fca791ccbbf
Alison Rowland <arowland@sunlightfoundation.com>
2011-04-01 14:22:05 -0400

    [#762] Remove accidentally committed duplicate file

--- sql_updates/762_create_whitehouse_visitors_table.sql
+++
@@ -1,9 +0,0 @@
-create table whitehouse_visitor (
-    id serial not null primary key,
-    first_name varchar(64),
-    middle_name varchar(64),
-    last_name varchar(64)
-);
-
-insert into whitehouse_visitor (first_name, middle_name, last_name)
-    select distinct first_name, middle_name, last_name from whitehouse_visitor_logs where description not ilike '%tour%' and description not ilike '%visitor%';
