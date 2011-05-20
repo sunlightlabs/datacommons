dcc22f6de8d8231866392dce22d7a69f9b9cdcbe
Alison Rowland <arowland@sunlightfoundation.com>
2011-05-20 12:26:04 -0400

    [#830] Broke models into separate "Contractor" and "Misconduct" tables.

--- dcdata/pogo/models.py
+++ dcdata/pogo/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 
-class ContractorMisconduct(models.Model):
-    contractor = models.CharField(max_length=255, blank=False, null=False)
+class Contractor(models.Model):
+    name = models.CharField(max_length=255, blank=False, null=False)
+    url = models.URLField(blank=False, null=False)
+
+class Misconduct(models.Model):
+    contractor = models.ForeignKey(Contractor)
     instance = models.CharField(max_length=255, blank=False, null=False)
 
     penalty_amount = models.BigIntegerField(null=True)
 
     contracting_party = models.CharField(max_length=255, blank=False, null=False)
-    court_type = models.CharField(max_length=14, blank=True, null=True)
+    court_type = models.CharField(max_length=20, blank=True, null=True)
 
     date = models.DateField()
     date_significance = models.CharField(max_length=50, blank=True, null=False)
@@ -16,4 +20,8 @@ class ContractorMisconduct(models.Model):
     disposition = models.CharField(max_length=30, blank=True, null=False)
     enforcement_agency = models.CharField(max_length=255, blank=True, null=False)
     misconduct_type = models.CharField(max_length=32, blank=False, null=False)
+
+    synopsis = models.TextField(blank=True)
+
+    url = models.URLField(blank=False, null=False)
 
