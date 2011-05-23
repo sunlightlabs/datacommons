128d2a4b669f2b3079460f94bf2880778032dbdb
Alison Rowland <arowland@sunlightfoundation.com>
2011-05-23 09:20:34 -0400

    [#831] Updates to TransparencyData handler. Cannot currently search on Contractor, so needs some tweaking.

--- dcapi/contractor_misconduct/handlers.py
+++ dcapi/contractor_misconduct/handlers.py
@@ -1,31 +1,32 @@
 from dcapi.common.handlers import FilterHandler
 from dcapi.common.schema import InclusionField, FulltextField, ComparisonField
 from dcapi.schema import Schema
-from dcdata.pogo.models import ContractorMisconduct
+from dcdata.pogo.models import Misconduct
 
 CONTRACTOR_MISCONDUCT_SCHEMA = Schema(
     InclusionField('date_year'),
 
     ComparisonField('penalty_amount', 'penalty_amount', cast=int),
 
-    FulltextField('contractor'),
-    FulltextField('enforcement_agency'),
-    FulltextField('instance'),
-    FulltextField('contracting_party'),
+    FulltextField('name_ft', ['name']),
+    FulltextField('enforcement_agency_ft'),
+    FulltextField('instance_ft'),
+    FulltextField('contracting_party_ft'),
 
 )
 
 
 def filter_contractor_misconduct(request):
-    return CONTRACTOR_MISCONDUCT_SCHEMA.build_filter(ContractorMisconduct.objects, request).order_by()
+    return CONTRACTOR_MISCONDUCT_SCHEMA.build_filter(Misconduct.objects, request).order_by()
 
 
 class ContractorMisconductFilterHandler(FilterHandler):
-    model = ContractorMisconduct
+    #fields = ( ('contractor', ('name',),), 'instance', 'penalty_amount', 'contracting_party', 'court_type', 'date', 'date_significance', 'date_year', 'disposition', 'enforcement_agency', 'misconduct_type', 'synopsis')
+    model = Misconduct
     ordering = ['-date','-disposition']
     filename = 'contractor_misconduct'
-        
+
     def queryset(self, params):
         return filter_contractor_misconduct(self._unquote(params))
-    
+
 
--- sql_updates/831_add_ft_indexes_for_pogo.sql
+++ sql_updates/831_add_ft_indexes_for_pogo.sql
@@ -1,4 +1,4 @@
-create index pogo_contractormisconduct__contractor_ft on pogo_contractormisconduct using gin(to_tsvector('datacommons', contractor));
-create index pogo_contractormisconduct__instance_ft on pogo_contractormisconduct using gin(to_tsvector('datacommons', instance));
-create index pogo_contractormisconduct__contracting_party_ft on pogo_contractormisconduct using gin(to_tsvector('datacommons', contracting_party));
-create index pogo_contractormisconduct__enforcement_agency_ft on pogo_contractormisconduct using gin(to_tsvector('datacommons', enforcement_agency));
+create index pogo_contractor__name_ft on pogo_contractor using gin(to_tsvector('datacommons', name));
+create index pogo_misconduct__instance_ft on pogo_misconduct using gin(to_tsvector('datacommons', instance));
+create index pogo_misconduct__contracting_party_ft on pogo_misconduct using gin(to_tsvector('datacommons', contracting_party));
+create index pogo_misconduct__enforcement_agency_ft on pogo_misconduct using gin(to_tsvector('datacommons', enforcement_agency));
