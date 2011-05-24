651858893f159f0f4b53844a771540924c75548a
Alison Rowland <arowland@sunlightfoundation.com>
2011-05-23 23:15:33 -0400

    [#833] This is done in the overall aggregates file now, with a different approach.

--- sql_updates/833_create_pogo_assoc.sql
+++
@@ -1,7 +0,0 @@
-create table assoc_pogo (entity_id uuid, misconduct_id integer);
-insert into assoc_pogo (entity_id, misconduct_id)
-    select distinct match_id, misconduct.id
-    from matching_pogo_matches_20110520_1717 matches
-    inner join pogo_misconduct misconduct
-    on subject_id = contractor_id
-    where confidence > 2;
