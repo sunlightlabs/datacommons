8d70188807c46603003a86defa0f83615de4cd1b
Alison Rowland <arowland@sunlightfoundation.com>
2011-05-25 11:59:56 -0400

    [#838] Change to a more appropriate namespace name for POGO contractor ids

--- sql_updates/838_populate_entityattributes_with_contractor_ext_ids.sql
+++ sql_updates/838_populate_entityattributes_with_contractor_ext_ids.sql
@@ -1,5 +1,5 @@
 insert into matchbox_entityattribute (entity_id, namespace, value) 
-    select match_id, 'urn:sunlight:pogo_contractor_url', contractor_ext_id 
+    select match_id, 'urn:pogo:contractor', contractor_ext_id 
     from matching_pogo_matches_20110523_1051 m 
     inner join pogo_contractor c on m.subject_id = c.id 
     where confidence > 2;
