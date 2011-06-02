c2974a5c6adee008913e07088d6976b7ada3f226
Alison Rowland <arowland@sunlightfoundation.com>
2011-06-02 11:15:39 -0400

    [#852] Fix up contractor misconduct columns in Transparency Data. Also fix issue with tab not highlighting.

--- dcapi/aggregates/pogo/handlers.py
+++ dcapi/aggregates/pogo/handlers.py
@@ -5,10 +5,10 @@ from dcapi.aggregates.handlers import EntityTopListHandler
 class TopContractorMisconductHandler(EntityTopListHandler):
 
     args = 'entity_id cycle'.split()
-    fields = 'cycle year contractor_entity contractor penalty_amount contracting_party instance court_type misconduct_type misconduct_url'.split()
+    fields = 'cycle year contractor_entity contractor penalty_amount contracting_party instance disposition misconduct_type misconduct_url'.split()
 
     stmt = """
-        select cycle, year, contractor_entity, contractor, penalty_amount, contracting_party, instance, court_type, misconduct_type, misconduct_url
+        select cycle, year, contractor_entity, contractor, penalty_amount, contracting_party, instance, disposition, misconduct_type, misconduct_url
         from agg_pogo_contractor_misconduct cm
         where
             contractor_entity = %s
