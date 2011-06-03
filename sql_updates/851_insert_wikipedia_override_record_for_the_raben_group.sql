3e92fac6a47c2ce8773d09cebb7e7e9b9dbf39e3
Alison Rowland <arowland@sunlightfoundation.com>
2011-06-03 14:28:15 -0400

    [#851] Fixing the bio for The Raben Group.

---
+++ sql_updates/851_insert_wikipedia_override_record_for_the_raben_group.sql
@@ -0,0 +1,7 @@
+insert into matchbox_sunlightinfo (entity_id, bio, bio_url, notes) values (
+    'b87099a2-0936-40f8-a4fe-0dc9e7504805',
+    'The Raben Group is a Washington, DC-based lobbying and consulting firm formed by former United States Assistant Attorney General Robert Raben. The Raben Group specializes in issues of law and public policy. The firm maintains offices in Washington and Los Angeles with over 35 employees.',
+    'http://en.wikipedia.org/wiki/The_Raben_Group',
+    '"Raben Group" != "The Raben Group" on Wikipedia. They personally emailed us about it. See ticket: http://redmine.sunlightlabs.com/redmine/issues/851'
+);
+
