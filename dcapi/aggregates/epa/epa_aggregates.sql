4abc1bfef6cbc60967edac5732cf205036ab84df
Alison Rowland <arowland@sunlightfoundation.com>
2011-06-30 17:51:03 -0400

    Add top EPA actions aggregate table

--- dcapi/aggregates/epa/epa_aggregates.sql
+++ dcapi/aggregates/epa/epa_aggregates.sql
@@ -83,47 +83,40 @@ create index agg_epa_echo_totals__entity_id on agg_epa_echo_totals (entity_id);
 
 -- Top 10 Instances
 
---select date_trunc('second', now()) || '-- Dropping aggregate table';
---drop table if exists agg_epa_echo_contractor_misconduct;
---
---select date_trunc('second', now()) || '-- Creating aggregate table';
---
---create table agg_epa_echo_contractor_misconduct as
---    with misconduct_by_cycle as (
---        select
---            date_year + date_year % 2 as cycle,
---            date_year as year,
---            date_significance,
---            entity.id as contractor_entity,
---            contractor.name as contractor,
---            contracting_party,
---            penalty_amount,
---            instance,
---            disposition,
---            misconduct_type,
---            misconduct.url as misconduct_url,
---            rank() over (partition by entity.id, date_year + date_year % 2 order by penalty_amount desc, instance) as rank
---        from pogo_misconduct misconduct
---        inner join pogo_contractor contractor on misconduct.contractor_id = contractor.id
---        inner join assoc_epa_echo assoc on assoc.misconduct_id = misconduct.id
---        inner join matchbox_entity entity on assoc.entity_id = entity.id
---    )
---    select cycle, year, date_significance, contractor_entity, contractor, contracting_party, penalty_amount, instance, disposition, misconduct_type, misconduct_url
---    from misconduct_by_cycle
---    where rank <= :agg_top_n
---
---    union all
---
---    select cycle, year, date_significance, contractor_entity, contractor, contracting_party, penalty_amount, instance, disposition, misconduct_type, misconduct_url
---    from (
---        select -1 as cycle, year, date_significance, contractor_entity, contractor, contracting_party, penalty_amount, instance, disposition, misconduct_type, misconduct_url,
---            rank() over (partition by contractor_entity order by penalty_amount desc) as rank
---        from misconduct_by_cycle
---    ) x
---    where rank <= :agg_top_n
---;
---
---select date_trunc('second', now()) || ' -- create index agg_epa_echo_contractor_misconduct__contractor_entity on agg_epa_echo_contractor_misconduct (contractor_entity)';
---create index agg_epa_echo_contractor_misconduct__contractor_entity on agg_epa_echo_contractor_misconduct (contractor_entity);
---
---
+select date_trunc('second', now()) || '-- Dropping aggregate table';
+drop table if exists agg_epa_echo_actions;
+
+select date_trunc('second', now()) || '-- Creating aggregate table';
+
+create table agg_epa_echo_actions as
+    with actions_by_cycle as (
+        select
+            cycle,
+            year,
+            defendant_entity,
+            defendant_name,
+            case_name,
+            amount,
+            -- location info
+            rank() over (partition by defendant_entity, cycle order by amount desc) as rank
+        from epa_echo_actions_view
+    )
+    select cycle, year, defendant_entity, defendant_name, case_name, amount
+    from actions_by_cycle
+    where rank <= :agg_top_n
+
+    union all
+
+    select cycle, year, defendant_entity, defendant_name, case_name, amount
+    from (
+        select -1 as cycle, year, defendant_entity, defendant_name, case_name, amount,
+            rank() over (partition by defendant_entity order by amount desc) as rank
+        from actions_by_cycle
+    ) x
+    where rank <= :agg_top_n
+;
+
+select date_trunc('second', now()) || ' -- create index agg_epa_echo_actions__defendant_entity on agg_epa_echo_actions (defendant_entity)';
+create index agg_epa_echo_actions__defendant_entity on agg_epa_echo_actions (defendant_entity);
+
+
