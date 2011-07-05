ea5b9d2492c7d7ac872e19ac38a870021344ce1b
Alison Rowland <arowland@sunlightfoundation.com>
2011-07-05 16:05:54 -0400

    [#880] Adding locations to EPA handler

--- dcapi/aggregates/epa/handlers.py
+++ dcapi/aggregates/epa/handlers.py
@@ -5,14 +5,27 @@ from dcapi.aggregates.handlers import EntityTopListHandler
 class TopViolationActionsHandler(EntityTopListHandler):
 
     args = 'entity_id cycle'.split()
-    fields = 'cycle case_name defendant_name defendant_entity amount year'.split()
+    fields = 'cycle case_id case_name defendant_name defendant_entity defendants_count other_defendants locations amount year'.split()
 
     stmt = """
-        select cycle, case_name, defendant_name, defendant_entity, amount, year--, location
-        from agg_epa_echo_actions
+        select
+            cycle,
+            case_id,
+            case_name,
+            defendant_name,
+            defendant_entity,
+            count(distinct defennm) as defendants_count,
+            array_to_string(array_agg(distinct d.defennm), ', ')as other_defendants,
+            array_to_string(array_agg(distinct f.fcltcit || ', ' || f.fcltstc), ', ') as locations,
+            amount,
+            year
+        from agg_epa_echo_actions a
+        inner join epa_echo_defendant d on a.case_id = d.enfocnu
+        inner join epa_echo_facility f on a.case_id = f.enfocnu
         where
             defendant_entity = %s
             and cycle = %s
+        group by cycle, case_id, case_name, defendant_name, defendant_entity, f.fcltcit, f.fcltstc, amount, year
         order by cycle desc, amount desc
     """.format(', '.join(fields))
 
