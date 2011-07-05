a2b82e4c2b249c3d0a28716e2ca2f1ae461d19e1
Alison Rowland <arowland@sunlightfoundation.com>
2011-07-05 17:32:52 -0400

    [#879] Add date significance to mouseover on date in EPA table

--- dcapi/aggregates/epa/epa_aggregates.sql
+++ dcapi/aggregates/epa/epa_aggregates.sql
@@ -36,18 +36,19 @@ drop view epa_echo_actions_view;
 select date_trunc('second', now()) || '-- Creating epa_echo_actions_view';
 create view epa_echo_actions_view as
     select
-        extract('year' from max(subacad))::integer + extract('year' from max(subacad))::integer % 2 as cycle,
-        extract('year' from max(subacad))::integer as year,
+        extract('year' from subacad)::integer + extract('year' from subacad)::integer % 2 as cycle,
+        extract('year' from subacad)::integer as year,
         assoc.entity_id as defendant_entity,
         max(defendant_name) as defendant_name,
         c.enfocnu as case_id,
         enfornm as case_name,
+        subacty as date_significance,
         coalesce(enfotpa, 0) + coalesce(enfcslp, 0) + coalesce(enfcraa, 0) as amount
     from epa_echo_case_identifier c
     inner join epa_echo_penalty p on c.enfocnu = p.enfocnu
-    inner join epa_echo_milestone m on m.enfocnu = c.enfocnu
+    inner join (select distinct on (enfocnu) enfocnu, subacad, subacty from epa_echo_milestone order by enfocnu, subacad desc) m on m.enfocnu = c.enfocnu
     inner join assoc_epa_echo assoc on assoc.case_id = c.id
-    group by entity_id, c.enfocnu, enfornm, enfotpa, enfcslp, enfcraa
+    group by entity_id, c.enfocnu, enfornm, enfotpa, enfcslp, enfcraa, subacad, subacty
 ;
 
 
@@ -99,19 +100,20 @@ create table agg_epa_echo_actions as
             case_id,
             max(case_name) as case_name,
             max(amount) as amount,
+            date_significance,
             rank() over (partition by defendant_entity, cycle order by max(amount) desc) as rank
         from epa_echo_actions_view
-        group by cycle, year, defendant_entity, case_id
+        group by cycle, year, defendant_entity, case_id, date_significance
     )
-    select cycle, year, defendant_entity, defendant_name, case_id, case_name, amount
+    select cycle, year, defendant_entity, defendant_name, case_id, case_name, amount, date_significance
     from actions_by_cycle
     where rank <= :agg_top_n
 
     union all
 
-    select cycle, year, defendant_entity, defendant_name, case_id, case_name, amount
+    select cycle, year, defendant_entity, defendant_name, case_id, case_name, amount, date_significance
     from (
-        select -1 as cycle, year, defendant_entity, defendant_name, case_id, case_name, amount,
+        select -1 as cycle, year, defendant_entity, defendant_name, case_id, case_name, amount, date_significance,
             rank() over (partition by defendant_entity order by amount desc) as rank
         from actions_by_cycle
     ) x
--- dcapi/aggregates/epa/handlers.py
+++ dcapi/aggregates/epa/handlers.py
@@ -5,7 +5,7 @@ from dcapi.aggregates.handlers import EntityTopListHandler
 class TopViolationActionsHandler(EntityTopListHandler):
 
     args = 'entity_id cycle'.split()
-    fields = 'cycle case_id case_name defendant_name defendant_entity defendants_count other_defendants locations amount year'.split()
+    fields = 'cycle case_id case_name defendant_name defendant_entity defendants_count other_defendants locations amount year date_significance'.split()
 
     stmt = """
         select
@@ -18,14 +18,15 @@ class TopViolationActionsHandler(EntityTopListHandler):
             array_to_string(array_agg(distinct d.defennm), ', ')as other_defendants,
             array_to_string(array_agg(distinct f.fcltcit || ', ' || f.fcltstc), ', ') as locations,
             amount,
-            year
+            year,
+            date_significance
         from agg_epa_echo_actions a
         inner join epa_echo_defendant d on a.case_id = d.enfocnu
         inner join epa_echo_facility f on a.case_id = f.enfocnu
         where
             defendant_entity = %s
             and cycle = %s
-        group by cycle, case_id, case_name, defendant_name, defendant_entity, f.fcltcit, f.fcltstc, amount, year
+        group by cycle, case_id, case_name, defendant_name, defendant_entity, f.fcltcit, f.fcltstc, amount, year, date_significance
         order by cycle desc, amount desc
     """.format(', '.join(fields))
 
