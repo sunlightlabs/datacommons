7a8af6e59c9bcbcfc3a01ab4d7fb1eed65b2312f
Andrew Pendleton <apendleton@sunlightfoundation.com>
2011-08-10 11:44:31 -0400

    Adding title to handlers; currently does a join at call time but probably should be pre-computed in the aggregates.

--- dcapi/aggregates/regulations/handlers.py
+++ dcapi/aggregates/regulations/handlers.py
@@ -3,26 +3,28 @@ from dcapi.aggregates.handlers import EntityTopListHandler
 
 class RegulationsTextHandler(EntityTopListHandler):
     
-    fields = "agency docket year count".split()
+    fields = "agency docket year count title".split()
     
     stmt = """
-        select agency, docket, cast(year as int) as year, count
-        from agg_regulations_text
+        select agg_regulations_text.agency as agency, docket, cast(year as int) as year, count, title
+        from agg_regulations_text, regulations_dockets
         where
             entity_id = %s
             and cycle = %s
+            and agg_regulations_text.docket = regulations_dockets.docket_id
         order by count desc
         limit %s"""
 
 class RegulationsSubmitterHandler(EntityTopListHandler):
 
-    fields = "agency docket year count".split()
+    fields = "agency docket year count title".split()
 
     stmt = """
-        select agency, docket, cast(year as int) as year, count
-        from agg_regulations_submitter
+        select agg_regulations_submitter.agency as agency, docket, cast(year as int) as year, count, title
+        from agg_regulations_submitter, regulations_dockets
         where
             entity_id = %s
             and cycle = %s
+            and agg_regulations_submitter.docket = regulations_dockets.docket_id
         order by count desc
         limit %s"""
