fd8c006ded6a069c67a8b7d8e87a11709692bce6
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2011-08-15 18:34:48 -0400

    moved more functionality from handler to aggregates

--- dcapi/aggregates/epa/epa_aggregates.sql
+++ dcapi/aggregates/epa/epa_aggregates.sql
@@ -24,9 +24,13 @@ create table epa_echo_actions as
 select i.enfocnu as case_num, max(enfornm) as case_name,
     extract('year' from (select min(subacad) from epa_echo_milestone m where m.enfocnu = i.enfocnu))::integer as min_year,
     extract('year' from (select max(subacad) from epa_echo_milestone m where m.enfocnu = i.enfocnu))::integer as max_year,
+    (select subacty from epa_echo_milestone m where m.enfocnu = i.enfocnu order by subacad limit 1) as min_year_significance,
+    (select subacty from epa_echo_milestone m where m.enfocnu = i.enfocnu order by subacad desc limit 1) as max_year_significance,
     (select count(*) from epa_echo_defendant d where d.enfocnu = i.enfocnu) as num_defendants,
-    'to do'::varchar(255) as max_year_significance,
-    (select sum(coalesce(enfccaa, 0) + coalesce(enfcraa, 0) + coalesce(enfotpa, 0) + coalesce(enfotsa, 0)) from epa_echo_penalty p where p.enfocnu = i.enfocnu) as penalty
+    (select sum(coalesce(enfccaa, 0) + coalesce(enfcraa, 0) + coalesce(enfotpa, 0) + coalesce(enfotsa, 0)) from epa_echo_penalty p where p.enfocnu = i.enfocnu) as penalty,
+    (select count(distinct defennm) from epa_echo_defendant d where d.enfocnu = i.enfocnu) as defendants_count,
+    (select array_to_string(array_agg(distinct d.defennm), ', ') from epa_echo_defendant d where d.enfocnu = i.enfocnu) as defendants,
+    (select array_to_string(array_agg(distinct f.fcltcit || ', ' || f.fcltstc), '; ') from epa_echo_facility f where f.enfocnu = i.enfocnu) as locations
 from epa_echo_case_identifier i
 group by i.enfocnu;
 
@@ -101,24 +105,27 @@ create table agg_epa_echo_actions as
             a.*,
             o.defendant_name,
             o.entity_id,
-            rank() over (partition by entity_id, cycle order by penalty desc) as rank
+            rank() over (partition by entity_id, cycle order by case when num_defendants = 1 then penalty else -1.0 / penalty end desc) as rank
         from epa_echo_relevant_actions a
         inner join assoc_epa_echo_org o using (case_num)
     )
 
-    select cycle, max_year, entity_id, case_num, case_name, defendant_name, penalty, max_year_significance
+    select cycle, case_num, case_name, defendant_name, entity_id, num_defendants, defendants, locations,
+        penalty, max_year, max_year_significance,
+        rank
     from actions_by_cycle
-    --where rank <= :agg_top_n
+    where rank <= :agg_top_n
 
     union all
 
-    select cycle, max_year, entity_id, case_num, case_name, defendant_name, penalty, max_year_significance
+    select *
     from (
-        select -1 as cycle, max_year, entity_id, case_num, case_name, defendant_name, penalty, max_year_significance,
-            rank() over (partition by entity_id order by penalty desc) as rank
+        select -1 as cycle, case_num, case_name, defendant_name, entity_id, num_defendants, defendants, locations,
+            penalty, max_year, max_year_significance,
+            rank() over (partition by entity_id order by case when num_defendants = 1 then penalty else -1.0 / penalty end desc) as rank
         from actions_by_cycle
     ) x
-    --where rank <= :agg_top_n
+    where x.rank <= :agg_top_n
 ;
 
 select date_trunc('second', now()) || ' -- create index agg_epa_echo_actions__defendant_entity on agg_epa_echo_actions (defendant_entity)';
--- dcapi/aggregates/epa/handlers.py
+++ dcapi/aggregates/epa/handlers.py
@@ -12,21 +12,18 @@ class TopViolationActionsHandler(EntityTopListHandler):
             case_name,
             defendant_name,
             entity_id,
-            count(distinct defennm) as defendants_count,
-            array_to_string(array_agg(distinct d.defennm), ', ')as other_defendants,
-            array_to_string(array_agg(distinct f.fcltcit || ', ' || f.fcltstc), '; ') as locations,
-            array_to_string(array_agg(distinct f.fcltyad || ', ' || f.fcltcit || ', ' || f.fcltstc), '; ') as location_addresses,
+            num_defendants,
+            defendants,
+            locations,
+            'forgot to include, do in next rebuild' as location_addresses,
             penalty,
             max_year,
             max_year_significance
         from agg_epa_echo_actions a
-        inner join epa_echo_defendant d on a.case_num = d.enfocnu
-        inner join epa_echo_facility f on a.case_num = f.enfocnu
         where
             entity_id = %s
             and cycle = %s
-        group by cycle, case_num, case_name, defendant_name, entity_id, penalty, max_year, max_year_significance
-        order by cycle desc, case when count(distinct defennm) = 1 then penalty else -1.0 / penalty end desc
+        order by rank
         limit %s
     """.format(', '.join(fields))
 
