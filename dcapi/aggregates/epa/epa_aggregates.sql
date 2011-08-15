77291513fd9310decd825942a472ba83e25c5f4f
Alison Rowland <arowland@sunlightfoundation.com>
2011-08-15 14:51:51 -0400

    Merged from dev

--- dcapi/aggregates/epa/epa_aggregates.sql
+++ dcapi/aggregates/epa/epa_aggregates.sql
@@ -13,55 +13,47 @@ create table epa_echo_org_map (
     ultorg_name varchar(255)
 );
 select date_trunc('second', now()) || '-- Copying org_map from file';
-\copy epa_echo_org_map from ~/work/epa/larrys_orgname_list.csv CSV HEADER
+\copy epa_echo_org_map from larrys_orgname_list.csv CSV HEADER
+
+
+-- relevant actions
+
+drop table if exists epa_echo_actions cascade;
+
+create table epa_echo_actions as
+select i.enfocnu as case_num, max(enfornm) as case_name,
+    extract('year' from (select min(subacad) from epa_echo_milestone m where m.enfocnu = i.enfocnu))::integer as min_year,
+    extract('year' from (select max(subacad) from epa_echo_milestone m where m.enfocnu = i.enfocnu))::integer as max_year,
+    (select count(*) from epa_echo_defendant d where d.enfocnu = i.enfocnu) as num_defendants,
+    'to do'::varchar(255) as max_year_significance,
+    (select sum(coalesce(enfccaa, 0) + coalesce(enfcraa, 0) + coalesce(enfotpa, 0) + coalesce(enfotsa, 0)) from epa_echo_penalty p where p.enfocnu = i.enfocnu) as penalty
+from epa_echo_case_identifier i
+group by i.enfocnu;
+
+
+drop view if exists epa_echo_relevant_actions;
+
+create view epa_echo_relevant_actions as
+    select *, max_year + max_year % 2 as cycle
+    from epa_echo_actions
+    where
+        min_year > 2000
+        and penalty > 1000000;
 
 
 -- Associations Table
 
 select date_trunc('second', now()) || '-- drop table assoc_epa_echo_org_ultorg';
-drop table if exists assoc_epa_echo_org_ultorg cascade;
+drop table if exists assoc_epa_echo_org cascade;
 
 select date_trunc('second', now()) || '-- create table assoc_epa_echo_org_ultorg';
-create table assoc_epa_echo_org_ultorg as
-    select
-        unnest(case_ids) as case_id,
-        org.id as org_entity,
-        ultorg.id as ultorg_entity
-    from
-        epa_echo_org_map m
-        left join matchbox_entity org on lower(trim(both from m.org_name)) = lower(org.name) and (org.type is null or org.type = 'organization')
-        left join matchbox_entity ultorg on lower(trim(both from m.ultorg_name)) = lower(ultorg.name) and (ultorg.type is null or ultorg.type = 'organization')
-;
-
-
--- Actions View
-
-select date_trunc('second', now()) || '-- Dropping epa_echo_actions_view';
-drop view epa_echo_actions_view;
-
-select date_trunc('second', now()) || '-- Creating epa_echo_actions_view';
-create view epa_echo_actions_view as
-    select
-        c.enfocnu as case_id,
-        max(enfornm) as case_name,
-        extract('year' from subacad)::integer + extract('year' from subacad)::integer % 2 as cycle,
-        extract('year' from subacad)::integer as year,
-        assoc.org_entity as defendant_entity,
-        max(oe.name) as defendant_name,
-        assoc.ultorg_entity as defendant_parent_entity,
-        max(ue.name) as defendant_parent_name,
-        subacty as date_significance,
-        coalesce(enfotpa, 0) + coalesce(enfcslp, 0) + coalesce(enfcraa, 0) as amount
-    from (select distinct on (enforcd) enfocnu, enfornm from epa_echo_case_identifier order by enforcd asc) c
-    inner join epa_echo_penalty p on c.enfocnu = p.enfocnu
-    inner join epa_echo_defendant d on c.enfocnu = d.enfocnu
-    inner join (select distinct on (enfocnu) enfocnu, subacad, subacty from epa_echo_milestone order by enfocnu, subacad desc) m on m.enfocnu = c.enfocnu
-    inner join assoc_epa_echo_org_ultorg assoc on assoc.case_id = c.enfocnu
-    left join matchbox_entity oe on assoc.org_entity = oe.id
-    left join matchbox_entity ue on assoc.ultorg_entity = ue.id
-    group by assoc.org_entity, assoc.ultorg_entity, c.enfocnu, m.subacad, m.subacty, p.enfotpa, p.enfcslp, p.enfcraa
-;
-
+create table assoc_epa_echo_org as
+    select d.enfocnu as case_num, max(d.defennm) as defendant_name, e.id as entity_id
+    from epa_echo_relevant_actions a
+    inner join epa_echo_defendant d on d.enfocnu = a.case_num
+    inner join epa_echo_org_map m on d.defennm = m.defendant_name
+    inner join matchbox_entity e on e.name = m.org_name or e.name = m.ultorg_name
+    group by d.enfocnu, e.id;
 
 
 -- Totals
@@ -74,23 +66,15 @@ select date_trunc('second', now()) || '-- Creating totals table';
 create table agg_epa_echo_totals as
     with cases_by_cycle as (
         select
-            defendant_entity as entity_id,
+            entity_id,
             cycle,
-            count(*) as count,
-            sum(amount) as amount
-        from (select distinct on (defendant_entity, case_id) defendant_entity, cycle, amount from epa_echo_actions_view order by defendant_entity, case_id) x
-        group by defendant_entity, cycle
-
-        union all
-
-        select
-            defendant_parent_entity as entity_id,
-            cycle,
-            count(*) as count,
-            sum(amount) as amount
-        from epa_echo_actions_view
-        group by defendant_parent_entity, cycle
+            count(*),
+            sum(penalty) as amount
+        from epa_echo_relevant_actions a
+        inner join assoc_epa_echo_org o using (case_num)
+        group by entity_id, cycle
     )
+    
     select entity_id, cycle, count, amount
     from cases_by_cycle
 
@@ -114,50 +98,30 @@ select date_trunc('second', now()) || '-- Creating aggregate table';
 create table agg_epa_echo_actions as
     with actions_by_cycle as (
         select
-            cycle,
-            year,
-            defendant_entity,
-            max(defendant_name) as defendant_name,
-            case_id,
-            max(case_name) as case_name,
-            max(amount) as amount,
-            date_significance,
-            rank() over (partition by defendant_entity, cycle order by max(amount) desc) as rank
-        from epa_echo_actions_view
-        group by cycle, year, defendant_entity, case_id, date_significance
-
-        union all
-
-        select
-            cycle,
-            year,
-            defendant_parent_entity as defendant_entity,
-            array_to_string(array_agg(defendant_name), ', ') as defendant_name,
-            case_id,
-            max(case_name) as case_name,
-            max(amount) as amount,
-            date_significance,
-            rank() over (partition by defendant_parent_entity, cycle order by max(amount) desc) as rank
-        from epa_echo_actions_view
-        group by cycle, year, defendant_parent_entity, case_id, date_significance
+            a.*,
+            o.defendant_name,
+            o.entity_id,
+            rank() over (partition by entity_id, cycle order by penalty desc) as rank
+        from epa_echo_relevant_actions a
+        inner join assoc_epa_echo_org o using (case_num)
     )
 
-    select cycle, year, defendant_entity, defendant_name, case_id, case_name, amount, date_significance
+    select cycle, max_year, entity_id, case_num, case_name, defendant_name, penalty, max_year_significance
     from actions_by_cycle
-    where rank <= :agg_top_n
+    --where rank <= :agg_top_n
 
     union all
 
-    select cycle, year, defendant_entity, defendant_name, case_id, case_name, amount, date_significance
+    select cycle, max_year, entity_id, case_num, case_name, defendant_name, penalty, max_year_significance
     from (
-        select -1 as cycle, year, defendant_entity, defendant_name, case_id, case_name, amount, date_significance,
-            rank() over (partition by defendant_entity order by amount desc) as rank
+        select -1 as cycle, max_year, entity_id, case_num, case_name, defendant_name, penalty, max_year_significance,
+            rank() over (partition by entity_id order by penalty desc) as rank
         from actions_by_cycle
     ) x
-    where rank <= :agg_top_n
+    --where rank <= :agg_top_n
 ;
 
 select date_trunc('second', now()) || ' -- create index agg_epa_echo_actions__defendant_entity on agg_epa_echo_actions (defendant_entity)';
-create index agg_epa_echo_actions__defendant_entity on agg_epa_echo_actions (defendant_entity);
+create index agg_epa_echo_actions__defendant_entity on agg_epa_echo_actions (entity_id);
 
 
--- dcapi/aggregates/epa/handlers.py
+++ dcapi/aggregates/epa/handlers.py
@@ -3,30 +3,30 @@ from dcapi.aggregates.handlers import EntityTopListHandler
 
 
 class TopViolationActionsHandler(EntityTopListHandler):
-    fields = 'cycle case_id case_name defendant_name defendant_entity defendants_count other_defendants locations location_addresses amount year date_significance'.split()
+    fields = 'cycle case_id case_name defendant_name entity_id defendants_count other_defendants locations location_addresses amount year date_significance'.split()
 
     stmt = """
         select
             cycle,
-            case_id,
+            case_num,
             case_name,
             defendant_name,
-            defendant_entity,
+            entity_id,
             count(distinct defennm) as defendants_count,
             array_to_string(array_agg(distinct d.defennm), ', ')as other_defendants,
             array_to_string(array_agg(distinct f.fcltcit || ', ' || f.fcltstc), '; ') as locations,
             array_to_string(array_agg(distinct f.fcltyad || ', ' || f.fcltcit || ', ' || f.fcltstc), '; ') as location_addresses,
-            amount,
-            year,
-            date_significance
+            penalty,
+            max_year,
+            max_year_significance
         from agg_epa_echo_actions a
-        inner join epa_echo_defendant d on a.case_id = d.enfocnu
-        inner join epa_echo_facility f on a.case_id = f.enfocnu
+        inner join epa_echo_defendant d on a.case_num = d.enfocnu
+        inner join epa_echo_facility f on a.case_num = f.enfocnu
         where
-            defendant_entity = %s
+            entity_id = %s
             and cycle = %s
-        group by cycle, case_id, case_name, defendant_name, defendant_entity, amount, year, date_significance
-        order by cycle desc, amount desc
+        group by cycle, case_num, case_name, defendant_name, entity_id, penalty, max_year, max_year_significance
+        order by cycle desc, case when count(distinct defennm) = 1 then penalty else -1.0 / penalty end desc
         limit %s
     """.format(', '.join(fields))
 
