cd5c39471b8339e62c119dbdd67c0d8ee8f7ec47
Andrew Pendleton <apendleton@sunlightfoundation.com>
2011-08-16 15:45:06 -0400

    Switching to boto instead of CSV and the Mturk command line tools, plus some other cleanup.

--- dcdata/management/commands/generate_mturk_data.py
+++ dcdata/management/commands/generate_mturk_data.py
@@ -5,8 +5,13 @@ from name_cleaver import *
 from django.template import Template, Context
 from django.db import connection
 from optparse import make_option
+from boto.mturk.question import QuestionForm
+from boto.mturk.connection import MTurkConnection
+from boto.mturk.qualification import Qualifications, PercentAssignmentsApprovedRequirement
+import datetime
+from django.conf import settings
 
-question_xml = """<?xml version="1.0" encoding="UTF-8"?>
+question_template = Template("""<?xml version="1.0" encoding="UTF-8"?>
 <QuestionForm xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2005-10-01/QuestionForm.xsd">
     <Overview>
         <Text>
@@ -19,10 +24,35 @@ question_xml = """<?xml version="1.0" encoding="UTF-8"?>
         <QuestionContent>
             <FormattedContent><![CDATA[
                 <h2>Metadata</h2>
-                ${metadata}
+                {% spaceless %}
+                <strong>Name:</strong> {{ standard_name }}<br />
+                <strong>Type:</strong> {{ entity.type|capfirst }}<br />
+                
+                {% if entity.type == 'politician' %}
+                <strong>Party:</strong> {{ metadata.party }}<br />
+                
+                {% if metadata.seat %}
+                <strong>Last office sought:</strong> {% if metadata.state %}{{ metadata.state }} {% endif %}{{ metadata.seat }}<br />
+                {% endif %}
+                {% if metadata.seat_held %}
+                <strong> Last office held:</strong> {% if metadata.state_held %}{{ metadata.state_held }} {% endif %}{{ metadata.seat_held }}<br />
+                {% endif %}
+                
+                {% endif %}
+                
+                {% if entity.type == 'organization' %}
+                <strong>Is Lobbying Firm:</strong> {% if entity.organization_metadata.lobbying_firm %}Yes{% else %}No{% endif %}<br />
+                <strong>Industry:</strong> {{ entity.organization_metadata.industry_entity|lower|capfirst }}<br />
+                {% endif %}
+                {% endspaceless %}
                 <hr />
+                
                 <h2>Wikipedia Description</h2>
-                ${wikipedia_bio}
+                {% spaceless %}
+                <strong>From Wikipedia Page <a href="{{ metadata.bio_url }}">{{ metadata.bio_url }}</a>:</strong><br /><br />
+                
+                {{ metadata.bio|safe }}
+                {% endspaceless %}
             ]]></FormattedContent>
         </QuestionContent>
         <AnswerSpecification>
@@ -46,61 +76,7 @@ question_xml = """<?xml version="1.0" encoding="UTF-8"?>
         </AnswerSpecification>
     </Question>
 </QuestionForm>
-"""
-
-properties = """
-######################################
-##  HIT Properties
-######################################
-
-title:Wikipedia match validation
-description:We have matched a set of entities in a database to descriptions pulled from Wikipedia via an automated process. Confirm that the match is correct.
-keywords:movie, survey
-reward:0.08
-assignments:3
-annotation:wikipedia_match
-
-######################################
-## HIT Timing Properties
-######################################
-
-# this Assignment Duration value is 60 * 60 = 1 hour
-assignmentduration:3600
-
-# this HIT Lifetime value is 60*60*24*7 = 7 days
-hitlifetime:604800
-
-# this Auto Approval period is 60*60*24*5 = 5 days
-autoapprovaldelay:432000
-"""
-
-metadata_template = Template("""{% spaceless %}
-<strong>Name:</strong> {{ standard_name }}<br />
-<strong>Type:</strong> {{ entity.type|capfirst }}<br />
-
-{% if entity.type == 'politician' %}
-<strong>Party:</strong> {{ entity.metadata.party }}<br />
-
-{% if entity.metadata.seat %}
-<strong>Last office sought:</strong> {% if entity.metadata.state %}{{ entity.metadata.state }} {% endif %}{{ entity.metadata.seat }}<br />
-{% endif %}
-{% if metadata.seat_held %}
-<strong> Last office held:</strong> {% if entity.metadata.state_held %}{{ entity.metadata.state_held }} {% endif %}{{ metadata.seat_held }}<br />
-{% endif %}
-
-{% endif %}
-
-{% if entity.type == 'organization' %}
-<strong>Is Lobbying Firm:</strong> {% if entity.organization_metadata.lobbying_firm %}Yes{% else %}No{% endif %}<br />
-<strong>Industry:</strong> {{ entity.organization_metadata.industry_entity|lower|capfirst }}<br />
-{% endif %}
-{% endspaceless %}""")
-
-wikipedia_bio_template = Template("""{% spaceless %}
-<strong>From Wikipedia Page <a href="{{ entity.metadata.bio_url }}">{{ entity.metadata.bio_url }}</a>:</strong><br /><br />
-
-{{ entity.metadata.bio|safe }}
-{% endspaceless %}""")
+""")
 
 standardizers = {
     'individual': IndividualNameCleaver,
@@ -123,68 +99,68 @@ substitutions = {
     'I': 'Independent'
 }
 
-def get_row(id):
+def get_hit_xml(id):
     entity = Entity.objects.get(id=id)
     
     standard_name = standardizers[entity.type](entity.name).parse().__str__()
     
     # make fields pretty
-    print entity.id, entity.type
-    
+    metadata = entity.metadata
     for key in ['seat', 'seat_held', 'party']:
-        if key in entity.metadata and entity.metadata[key] in substitutions:
-            entity.metadata[key] = substitutions[entity.metadata[key]]
+        if key in metadata and metadata[key] in substitutions:
+            metadata[key] = substitutions[metadata[key]]
     
-    context = Context({'standard_name': standard_name, 'entity': entity})
+    context = Context({'standard_name': standard_name, 'entity': entity, 'metadata': metadata})
     
-    return {
-        'id': entity.id,
-        'name': standard_name,
-        'type': entity.type,
-        'metadata': metadata_template.render(context).encode('utf-8'),
-        'wikipedia_bio': wikipedia_bio_template.render(context).encode('utf-8'),
-    }
+    return question_template.render(context).encode('utf-8')
 
-def ensure_directory(dir):
-    if not (os.path.exists(dir) and os.path.isdir(dir)):
-        os.mkdir(dir)
+class FakeQuestionForm(QuestionForm):
+    def __init__(self, xml):
+        self.full_xml = xml
+    
+    def is_valid(self):
+        return True
+    
+    def get_as_xml(self):
+        return self.full_xml
+    
+    # hack to make it boolean-ize correctly
+    def __nonzero__(self):
+        return True
 
 class Command(BaseCommand):
-    args = '<directory>'
-    help = 'Collapses files into one directory'
     option_list = BaseCommand.option_list + (
         make_option('-n', '--number',
             action='store',
             dest='count',
             default=1000,
             help='Number of tasks to create.'),
+        make_option('-d', '--delete',
+            action='store_true',
+            dest='delete_first',
+            default=False,
+            help='Delete all old records before creating new ones.'),
+        make_option('-s', '--sandbox',
+            action='store_true',
+            dest='sandbox',
+            default=False,
+            help='Use the sandbox server instead of the production one.')
     )
     
     def handle(self, *args, **options):
-        if len(args) != 1:
-            raise CommandError('Please specify a directory.')
+        # create a connection
+        mturk = MTurkConnection(
+            getattr(settings, 'MTURK_AWS_KEY', settings.MEDIASYNC['AWS_KEY']),
+            getattr(settings, 'MTURK_AWS_SECRET', settings.MEDIASYNC['AWS_SECRET']),
+            host = 'mechanicalturk.sandbox.amazonaws.com' if options['sandbox'] else 'mechanicalturk.amazonaws.com'
+        )
         
-        directory = args[0]
+        # if --delete, delete all the old ones first.
+        if options['delete_first']:
+            for hit in mturk.get_all_hits():
+                mturk.disable_hit(hit.HITId)
         
-        # check that directories exists
-        ensure_directory(directory)
-        os.chdir(directory)
-        
-        # output plain files first
-        xml_file = open('wikipedia_matches.question', 'w')
-        xml_file.write(question_xml)
-        xml_file.close()
-        
-        props_file = open('wikipedia_matches.properties', 'w')
-        props_file.write(properties)
-        props_file.close()
-        
-        # output CSV
-        csv_file = open('wikipedia_matches.input', 'wb')
-        csv_writer = csv.DictWriter(csv_file, ['id', 'name', 'type', 'metadata', 'wikipedia_bio'], delimiter="\t")
-        csv_writer.writeheader()
-        
-        # get rows and generate metadata text
+        # iterate over items and create them one by one
         cursor = connection.cursor()
         cursor.execute("""
         select entity_id from matchbox_wikipediainfo where entity_id not in (select entity_id from matchbox_sunlightinfo) and bio != '' and bio is not null order by entity_id limit %s;
@@ -192,6 +168,17 @@ class Command(BaseCommand):
         [options['count']])
         
         for row in cursor:
-            csv_writer.writerow(get_row(row[0]))
-        
-        csv_file.close()
+            hit = mturk.create_hit(
+                question = FakeQuestionForm(get_hit_xml(row[0])),
+                max_assignments = 3,
+                annotation = row[0],
+                
+                title = "Wikipedia match validation",
+                description = "We have matched a set of entities in a database to descriptions pulled from Wikipedia via an automated process. Confirm that the match is correct.",
+                reward = 0.08,
+                duration = datetime.timedelta(days=7),
+                keywords = ['movie', 'survey'],
+                approval_delay = datetime.timedelta(days=5),
+                qualifications = Qualifications([PercentAssignmentsApprovedRequirement("GreaterThan", 90)])
+            )
+            print hit[0].HITId
