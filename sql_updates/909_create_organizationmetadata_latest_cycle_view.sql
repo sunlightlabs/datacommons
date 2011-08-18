a76b7c985370612733bdb084e46e2eff26378f7f
Alison Rowland <arowland@sunlightfoundation.com>
2011-08-18 15:50:51 -0400

    [#909] Limit the organization metadata latest cycle view to older cycles to avoid empty all cycles metadata

--- sql_updates/909_create_organizationmetadata_latest_cycle_view.sql
+++ sql_updates/909_create_organizationmetadata_latest_cycle_view.sql
@@ -6,5 +6,6 @@ create or replace view organization_metadata_latest_cycle_view as
         parent_entity_id,
         industry_entity_id
     from matchbox_organizationmetadata
+    where cycle < 2012
     order by entity_id, cycle desc
 ;
