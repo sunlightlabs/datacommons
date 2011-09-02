2ff711b12e6c0ac4c48577f1c3d78747f644b212
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2011-09-02 17:57:12 -0400

    initial changes to account for new regulations_text_matches. Not done

--- dcapi/aggregates/regulations/regulations_aggregates.sql
+++ dcapi/aggregates/regulations/regulations_aggregates.sql
@@ -1,15 +1,22 @@
 \set agg_top_n 10
 
 
+create view agg_regulations_collapsed_matches as
+select document_id, entity_id
+from regulations_text_matches
+group by document_id, entity_id;
+
+
 DROP TABLE IF EXISTS agg_regulations_text;
 CREATE TABLE agg_regulations_text as
 WITH ranked_aggregates as (
     SELECT
-        entity_id, agency, docket_id, extract(year from date_posted) as year, count(*),
+        entity_id, d.docket_id, d.title, d.agency, extract(year from d.date) as year, count(*),
         rank() over (PARTITION BY entity_id ORDER BY count(*) desc) as rank
     FROM regulations_comments_full
-    INNER JOIN regulations_text_matches USING (document_id)
-    GROUP BY entity_id, agency, docket_id, extract(year from date_posted)
+    INNER JOIN agg_regulations_collapsed_matches USING (document_id)
+    INNER JOIN regulations_dockets d USING (docket_id)
+    GROUP BY entity_id, d.docket_id, d.title, d.agency, extract(year from d.date)
 )
 SELECT entity_id, -1 as cycle, agency, docket_id as docket, year, count
 FROM ranked_aggregates
@@ -22,7 +29,7 @@ WITH totals as (
     SELECT
         entity_id, docket_id as docket, count(*)
     FROM regulations_comments_full
-    INNER JOIN regulations_text_matches USING (document_id)
+    INNER JOIN agg_regulations_collapsed_matches USING (document_id)
     GROUP BY entity_id, docket_id
 )
 SELECT
@@ -37,11 +44,12 @@ DROP TABLE IF EXISTS agg_regulations_submitter;
 CREATE TABLE agg_regulations_submitter as
 WITH ranked_aggregates as (
     SELECT
-        entity_id, agency, docket_id, extract(year from date_posted) as year, count(*),
+        entity_id, d.agency, d.docket_id, extract(year from d.date) as year, count(*),
         rank() over (PARTITION BY entity_id ORDER BY count(*) desc) as rank
     FROM regulations_comments_full
     INNER JOIN regulations_submitter_matches USING (document_id)
-    GROUP BY entity_id, agency, docket_id, extract(year from date_posted)
+    INNER JOIN regulations_dockets d USING (docket_id)
+    GROUP BY entity_id, d.agency, d.docket_id, extract(year from d.date)
 )
 SELECT entity_id, -1 as cycle, agency, docket_id as docket, year, count
 FROM ranked_aggregates
