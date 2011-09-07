eef232a46c43886d2e51a088c2e687f0dec6341b
Andrew Pendleton <apendleton@sunlightfoundation.com>
2011-09-06 23:16:58 -0400

    Adding handler behavior for dealing with the multiple-rows-per-document regs schema.

--- dcapi/aggregates/regulations/handlers.py
+++ dcapi/aggregates/regulations/handlers.py
@@ -31,16 +31,17 @@ class RegulationsSubmitterHandler(EntityTopListHandler):
 
 class RegulationsDocketTextHandler(EntityTopListHandler):
     
-    fields = "document_id title type date_posted".split()
+    fields = "document_id title type date_posted files".split()
     args = ['entity_id', 'docket_id', 'limit']
     
     stmt = """
-        select regulations_comments_full.document_id as document_id, title, type, date_posted
+        select regulations_text_matches.document_id as document_id, min(title) as title, min(type) as type, min(date_posted) as date_posted, array_agg('objectId=' || object_id || '&disposition=inline&contentType=' || file_type) as files
         from regulations_comments_full, regulations_text_matches
         where
             regulations_comments_full.document_id = regulations_text_matches.document_id
             and entity_id = %s
             and docket_id = %s
+        group by regulations_text_matches.document_id
         order by date_posted desc
         limit %s"""
 
