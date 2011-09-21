b645005b653905eea0db3e75a7b862764ecf7472
Alison Rowland <arowland@sunlightfoundation.com>
2011-09-21 17:46:14 -0400

    [#923] Adding some association-ish tables and two matching scripts

--- dcapi/aggregates/contributions/bundle_aggregates.sql
+++ dcapi/aggregates/contributions/bundle_aggregates.sql
@@ -33,16 +33,61 @@ create table tmp_overlapping_or_amended_bundles as
         and a.committee_fec_id = b.committee_fec_id
         and a.file_num != b.file_num
         and (b.filing_date > a.filing_date or (b.is_amendment and b.filing_date = a.filing_date))
-        and (a.start_date, a.end_date) overlaps (b.start_date, b.end_date) 
+        and (a.start_date, a.end_date) overlaps (b.start_date, b.end_date)
 ;
 
+-- TODO: check if this is coming out right
 update contribution_bundle cb
-set supercedes_id = file_num_a 
-from tmp_overlapping_or_amended_bundles o 
+set supercedes_id = file_num_a
+from tmp_overlapping_or_amended_bundles o
 where o.file_num_b = cb.file_num
 ;
 update contribution_bundle cb
 set should_ignore = 't'
-from tmp_overlapping_or_amended_bundles o 
+from tmp_overlapping_or_amended_bundles o
 where o.file_num_a = cb.file_num
 ;
+
+-- NOTE: The table below, right now, is matchbox_committeerecipient (committee_id, entity_id)
+-- QUESTIONS: What do committee recipients look like? What type of entity (politicions, other committees)
+-- are they, and do they change over time? Do we need to key them by cycle or file_num?
+-- The command to build this table is:
+-- insert into matchbox_committeerecipient (committee_id, entity_id) select distinct c.id, entity_id from contribution_contribution ctb inner join recipient_associations using (transaction_id) inner join matchbox_committee c on ctb.committee_ext_id = c.fec_id where ctb.contributor_type = 'C' and cycle >= 2008 and transaction_namespace = 'urn:fec:transaction' and recipient_type in ('P', 'C');
+
+--create table assoc_bundle_committee_to_recipient as
+--    select distinct
+--        committee_ext_id,
+--        entity_id
+--    from
+--        contribution_contribution
+--        inner join recipient_associations using (transaction_id)
+--;
+
+create table assoc_bundle_recipients as
+    select
+        file_num as report_id,
+        entity_id as recipient_entity_id
+    from
+        contribution_bundle
+        inner join matchbox_committee on committee_fec_id = fec_id
+        inner join matchbox_committeerecipient using (committee_id)
+;
+
+-- this is the query that's having trouble finishing.
+--insert into matchbox_committeerecipient (committee_id, entity_id) select distinct c.id, entity_id from contribution_contribution ctb inner join recipient_associations using (transaction_id) inner join matchbox_committee c on ctb.committee_ext_id = c.fec_id where ctb.contributor_type = 'C' and cycle >= 2008 and transaction_namespace = 'urn:fec:transaction' and recipient_type in ('P', 'C');
+
+
+create table assoc_bundle_donors as
+    select
+        id as bundle_id,
+        le.id as lobbyist_entity_id,
+        lfe.id as lobbying_firm_entity_id
+    from
+        contribution_lobbyistbundle lb
+        -- it would be nice if the table below looked more like assoc_bundler_lobbyist_donor
+        left join bundler_lobbyist_matches_curated lmc on lb.id = lmc.subject_id
+        -- this table (or similar) has yet to be created
+        left join assoc_bundle_lobbyist_employer lfe on lmc.id = lfe.id
+;
+
+
