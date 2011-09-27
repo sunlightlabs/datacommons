f0cafd9649b6af48805c4c8ff3bdc11f147eadfb
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2011-09-27 16:27:47 -0400

    removed committee URL field. Updated handlers to use aggregates

--- dcapi/aggregates/faca/faca_aggregate.sql
+++ dcapi/aggregates/faca/faca_aggregate.sql
@@ -68,18 +68,17 @@ create table agg_faca_records as
 with faca_most_recent as 
     (select 
         org_id, agency_abbr, agency_name, committee_name, member_firstlast,
-        first_value(committee_url) over committee_window as committee_url,
         first_value(chair) over member_window as chair,
         first_value(affiliation) over member_window as affiliation, 
         start_date, 
         end_date
     from faca_records
     window 
-        member_window as (partition by org_id, agency_abbr, agency_name, committee_name, member_firstlast order by end_date desc),
-        committee_window as (partition by org_id, agency_abbr, agency_name, committee_name order by end_date desc))
-select org_id, agency_abbr, agency_name, committee_name, committee_url, member_firstlast, chair, affiliation, min(start_date) as start_date, max(end_date) as end_date
+        member_window as (partition by org_id, agency_abbr, agency_name, committee_name, member_firstlast order by end_date desc))
+select org_id, agency_abbr, agency_name, committee_name, member_firstlast as member_name, chair, affiliation, min(start_date) as start_date, max(end_date) as end_date
 from faca_most_recent
-group by org_id, agency_abbr, agency_name, committee_name, committee_url, member_firstlast, chair, affiliation;
+group by org_id, agency_abbr, agency_name, committee_name, member_firstlast, chair, affiliation;
 
 create index agg_faca_records_org_idx on agg_faca_records (org_id);
 create index agg_faca_records_agency_org_idx on agg_faca_records(org_id, agency_abbr);
+
--- dcapi/aggregates/faca/handlers.py
+++ dcapi/aggregates/faca/handlers.py
@@ -8,7 +8,7 @@ class FACAAgenciesHandler(EntityTopListHandler):
     
     stmt = """
         select agency_abbr, agency_name, count(distinct member_name) as member_count, count(distinct committee_name) as committee_count
-        from faca_records, (values (%s::uuid, %s::integer)) as params (entity_id, cycle)
+        from agg_faca_records, (values (%s::uuid, %s::integer)) as params (entity_id, cycle)
         where
             org_id = params.entity_id
             and (params.cycle = -1 or params.cycle between extract(year from start_date) and extract(year from end_date) + 1)
@@ -21,16 +21,16 @@ class FACACommitteeMembersHandler(TopListHandler):
     
     args = ['entity_id', 'agency', 'cycle']
     
-    fields = ['committee_name', 'committee_url', 'member_name', 'chair', 'affiliation', 'start_date', 'end_date']
+    fields = ['committee_name', 'member_name', 'chair', 'affiliation', 'start_date', 'end_date']
     
     stmt = """
-        select committee_name, committee_url, member_name, chair, affiliation, min(start_date) as start_date, max(end_date) as end_date
-        from faca_records, (values (%s::uuid, %s, %s::integer)) as params (entity_id, agency, cycle)
+        select committee_name, member_name, chair, affiliation, min(start_date) as start_date, max(end_date) as end_date
+        from agg_faca_records, (values (%s::uuid, %s, %s::integer)) as params (entity_id, agency, cycle)
         where
             org_id = params.entity_id
             and agency_abbr = params.agency
             and (params.cycle = -1 or params.cycle between extract(year from start_date) and extract(year from end_date) + 1)
-        group by committee_name, committee_url, member_name, chair, affiliation
+        group by committee_name, member_name, chair, affiliation
         order by committee_name, member_name, start_date
     """
     
@@ -38,10 +38,9 @@ class FACACommitteeMembersHandler(TopListHandler):
         results = super(FACACommitteeMembersHandler, self).read(request, **kwargs)
         
         grouped = [{
-           'committee_name': item[0][0],
-           'committee_url': item[0][1],
+           'committee_name': item[0],
            'memberships': list(item[1]),
-        } for item in itertools.groupby(results, lambda x: (x['committee_name'], x['committee_url']))]
+        } for item in itertools.groupby(results, lambda x: x['committee_name'])]
         
         grouped_and_sorted = sorted(grouped, key=lambda x: len(x['memberships']), reverse=True)
         
