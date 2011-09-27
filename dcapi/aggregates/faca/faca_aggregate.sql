477916dace1c5186e044ce840a8112cb9f11060f
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2011-09-27 12:42:18 -0400

    added committee URL to aggreates, fixed bugs

--- dcapi/aggregates/faca/faca_aggregate.sql
+++ dcapi/aggregates/faca/faca_aggregate.sql
@@ -8,13 +8,14 @@ drop index if exists faca_records_affiliation_ft;
 delete from faca_records;
     
 insert into faca_records 
-    (agency_abbr, agency_name, committee_name, member_name, affiliation, 
+    (agency_abbr, agency_name, committee_name, committee_url, member_name, member_firstlast, affiliation, 
     chair, org_name, org_id, appointment_type, appointment_term, pay_plan,
     pay_source, member_designation, represented_group, start_date, end_date)
 select 
     AgencyAbbr as agency_abbr, 
     AgencyName as agency_name, 
-    CommitteeName as committee_name, 
+    CommitteeName as committee_name,
+    case when CommitteeURL is null then '' else CommitteeURL end as committee_url,
     replace(m.FirstName || ' ' || (case when m.MiddleName != '' then m.MiddleName || ' ' else '' end) || m.LastName, '.', '') as member_name, 
     replace(m.FirstName || ' ' || m.LastName, '.', '') as member_firstlast, 
     OccupationOrAffiliation as affiliation,
@@ -32,12 +33,14 @@ select
 from faca_committees
 inner join faca_agencies using (AID)
 inner join faca_members m using (CID)
+left join faca_generalinformation g using (CID)
 left join faca_matches using (MembersID)
 left join matchbox_entity e on entity_id = e.id
 group by 
     AgencyAbbr, 
     AgencyName, 
     CommitteeName, 
+    case when CommitteeURL is null then '' else CommitteeURL end,
     replace(m.FirstName || ' ' || (case when m.MiddleName != '' then m.MiddleName || ' ' else '' end) || m.LastName, '.', ''),
     replace(m.FirstName || ' ' || m.LastName, '.', ''),
     OccupationOrAffiliation,
@@ -64,16 +67,19 @@ drop table if exists agg_faca_records;
 create table agg_faca_records as
 with faca_most_recent as 
     (select 
-        org_id, agency_abbr, agency_name, committee_name, member_firstlast, 
+        org_id, agency_abbr, agency_name, committee_name, member_firstlast,
+        first_value(committee_url) over committee_window as committee_url,
         first_value(chair) over member_window as chair,
         first_value(affiliation) over member_window as affiliation, 
         start_date, 
         end_date
     from faca_records
-    window member_window as (partition by org_id, agency_abbr, agency_name, committee_name, member_firstlast order by end_date desc))
-select org_id, agency_abbr, agency_name, committee_name, member_firstlast, chair, affiliation, min(start_date) as start_date, max(end_date) as end_date
+    window 
+        member_window as (partition by org_id, agency_abbr, agency_name, committee_name, member_firstlast order by end_date desc),
+        committee_window as (partition by org_id, agency_abbr, agency_name, committee_name order by end_date desc))
+select org_id, agency_abbr, agency_name, committee_name, committee_url, member_firstlast, chair, affiliation, min(start_date) as start_date, max(end_date) as end_date
 from faca_most_recent
-group by org_id, agency_abbr, agency_name, committee_name, member_firstlast, chair, affiliation;
+group by org_id, agency_abbr, agency_name, committee_name, committee_url, member_firstlast, chair, affiliation;
 
 create index agg_faca_records_org_idx on agg_faca_records (org_id);
 create index agg_faca_records_agency_org_idx on agg_faca_records(org_id, agency_abbr);
