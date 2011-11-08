c313043bca724ae4f8cccea5759d2adb3f3344cf
Alison Rowland <arowland@sunlightfoundation.com>
2011-11-08 17:00:09 -0500

    [#988] Fix AP Moller Maersk entity name

---
+++ sql_updates/988_fix_ap_moller_maersk_name.sql
@@ -0,0 +1,2 @@
+update matchbox_entity set name = 'AP Moller-Maersk' where id = '42dcdc63241e4679b13a96f9d42c216b';
+update matchbox_entityalias set alias = 'AP Moller-Maersk' where entity_id = '42dcdc63241e4679b13a96f9d42c216b';
