fefb228b8cf0ae741d1489a11a621d70efe9753e
Andrew Pendleton <apendleton@sunlightfoundation.com>
2011-12-12 17:43:14 -0500

    Gutting all of the front-facing parts of TD, and killing dc_web.  Yay.

---
+++ public/urls.py
@@ -0,0 +1,58 @@
+from dcapi.contracts.urls import contractsfilter_handler
+from dcapi.contributions.urls import contributionfilter_handler
+from dcapi.earmarks.urls import earmarkfilter_handler
+from dcapi.grants.urls import grantsfilter_handler
+from dcapi.lobbying.urls import lobbyingfilter_handler
+from dcapi.contractor_misconduct.urls import contractor_misconduct_filter_handler
+from dcapi.epa.urls import epafilter_handler
+from dcapi.faca.urls import facafilter_handler
+from dcapi.contributions.bundling.urls import bundlingfilter_handler
+
+from django.conf.urls.defaults import patterns, url
+
+urlpatterns = patterns('dc_web.public.views',        
+    # contracts
+    url(r'^download/contracts/$', 'search_download', {'search_resource': contractsfilter_handler},  name="data_contracts_download"),
+    url(r'^data/contracts/$', 'search_preview', {'search_resource': contractsfilter_handler}, name="data_contracts"),
+    url(r'^data/contracts/count/$', 'search_count', {'search_resource': contractsfilter_handler}, name="data_contracts_count"),
+    
+    # contributions
+    url(r'^download/contributions/$', 'search_download', {'search_resource': contributionfilter_handler},  name="data_contributions_download"),
+    url(r'^data/contributions/$', 'search_preview', {'search_resource': contributionfilter_handler}, name="data_contributions"),
+    url(r'^data/contributions/count/$', 'search_count', {'search_resource': contributionfilter_handler}, name="data_contributions_count"),
+    
+    # earmarks
+    url(r'^download/earmarks/$', 'search_download', {'search_resource': earmarkfilter_handler},  name="data_earmarks_download"),
+    url(r'^data/earmarks/$', 'search_preview', {'search_resource': earmarkfilter_handler}, name="data_earmarks"),
+    url(r'^data/earmarks/count/$', 'search_count', {'search_resource': earmarkfilter_handler}, name="data_earmarks_count"),
+     
+    # grants
+    url(r'^download/grants/$', 'search_download', {'search_resource': grantsfilter_handler},  name="data_grants_download"),
+    url(r'^data/grants/$', 'search_preview', {'search_resource': grantsfilter_handler}, name="data_grants"),
+    url(r'^data/grants/count/$', 'search_count', {'search_resource': grantsfilter_handler}, name="data_grants_count"),
+
+    # lobbying
+    url(r'^download/lobbying/$', 'search_download', {'search_resource': lobbyingfilter_handler},  name="data_lobbying_download"),
+    url(r'^data/lobbying/$', 'search_preview', {'search_resource': lobbyingfilter_handler}, name="data_lobbying"),
+    url(r'^data/lobbying/count/$', 'search_count', {'search_resource': lobbyingfilter_handler}, name="data_lobbying_count"),
+
+    # contractor_misconduct
+    url(r'^download/contractor_misconduct/$', 'search_download', {'search_resource': contractor_misconduct_filter_handler},  name="data_contractor_misconduct_download"),
+    url(r'^data/contractor_misconduct/$', 'search_preview', {'search_resource': contractor_misconduct_filter_handler}, name="data_contractor_misconduct"),
+    url(r'^data/contractor_misconduct/count/$', 'search_count', {'search_resource': contractor_misconduct_filter_handler}, name="data_contractor_misconduct_count"),
+
+    # epa_echo
+    url(r'^download/epa_echo/$', 'search_download', {'search_resource': epafilter_handler},  name="data_epa_echo_download"),
+    url(r'^data/epa_echo/$', 'search_preview', {'search_resource': epafilter_handler}, name="data_epa_echo"),
+    url(r'^data/epa_echo/count/$', 'search_count', {'search_resource': epafilter_handler}, name="data_epa_echo_count"),
+
+    # faca
+    url(r'^download/faca/$', 'search_download', {'search_resource': facafilter_handler},  name="data_faca_download"),
+    url(r'^data/faca/$', 'search_preview', {'search_resource': facafilter_handler}, name="data_faca"),
+    url(r'^data/faca/count/$', 'search_count', {'search_resource': facafilter_handler}, name="data_faca_count"),
+
+    # bundled contributions
+    url(r'^download/contributions/bundled/$', 'search_download', {'search_resource': bundlingfilter_handler},  name="data_bundling_download"),
+    url(r'^data/contributions/bundled/$', 'search_preview', {'search_resource': bundlingfilter_handler}, name="data_bundling"),
+    url(r'^data/contributions/bundled/count/$', 'search_count', {'search_resource': bundlingfilter_handler}, name="data_bundling_count"),        
+)
--- urls.py
+++ urls.py
@@ -1,17 +1,18 @@
+from django.conf import settings
 from django.conf.urls.defaults import *
+from django.contrib import admin
+from django.views.generic.simple import direct_to_template
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^app/', include('app.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/1.0/', include('dcapi.urls')),
+    url(r'^api/locksmith/', include('locksmith.auth.urls')),
+    url(r'^', include('public.urls')),
 )
+
+if (settings.DEBUG):  
+    urlpatterns += patterns('',  
+        url(r'^%s/(?P<path>.*)$' % settings.MEDIA_URL.strip('/'), 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),  
+    )
