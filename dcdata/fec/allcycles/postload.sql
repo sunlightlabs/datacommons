6249ad4a4e6e3730b0baf742312bd6230a75368f
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2012-03-26 14:32:56 -0700

    added creation of all main FEC files 2000-2012

--- dcdata/fec/allcycles/postload.sql
+++ dcdata/fec/allcycles/postload.sql
@@ -1,6 +1,128 @@
 
--- used only in the "One Percent of One Percent" project
--- alter table fec_pac_summaries add column cycle integer;
+create view fec_candidates_allcycles_import as
+    select 2012 as cycle, * from fec_candidates_12
+union all
+    select 2010, * from fec_candidates_10
+union all
+    select 2008, * from fec_candidates_08
+union all
+    select 2006, * from fec_candidates_06
+union all
+    select 2004, * from fec_candidates_04
+union all
+    select 2002, * from fec_candidates_02
+union all
+    select 2000, * from fec_candidates_00;
+
+drop table if exists fec_candidates_allcycles;
+create table fec_candidates_allcycles as
+select *,
+    case
+        when substring(candidate_id for 1) = 'P' then 'P'
+        when substring(candidate_id for 1) = 'S' then 'S' || '-' || substring(candidate_id from 3 for 2)
+        when substring(candidate_id for 1) = 'H' then 'H' || '-' || substring(candidate_id from 3 for 2) || '-' || current_district
+    end as race
+from fec_candidates_allcycles_import;
+
+create index fec_candidates_allcycles_candidate_id on fec_candidates_allcycles (candidate_id);
+
+
+create table fec_committees_allcycles as
+    select 2012 as cycle, * from fec_committees_12
+union all
+    select 2010, * from fec_committees_10
+union all
+    select 2008, * from fec_committees_08
+union all
+    select 2006, * from fec_committees_06
+union all
+    select 2004, * from fec_committees_04
+union all
+    select 2002, * from fec_committees_02
+union all
+    select 2000, * from fec_committees_00;
+
+
+create view fec_indiv_allcycles_import as
+    select 2012 as cycle, * from fec_indiv_12
+union all
+    select 2010, * from fec_indiv_10
+union all
+    select 2008, * from fec_indiv_08
+union all
+    select 2006, * from fec_indiv_06
+union all
+    select 2004, * from fec_indiv_04
+union all
+    select 2002, * from fec_indiv_02
+union all
+    select 2000, * from fec_indiv_00;
+
+drop table if exists fec_indiv_allcycles;
+create table fec_indiv_allcycles as
+select cycle, fec_record, filer_id, amendment, transaction_type, contributor_lender_transfer as contributor_name, city, state, zipcode, election_type,
+    regexp_replace(occupation, '/[^/]*$', '') as organization, regexp_replace(occupation, '^.*/', '') as occupation,
+    (transaction_century || transaction_year || transaction_month || transaction_day)::date as date,
+    overpunch(amount) as amount
+from fec_indiv_allcycles_import;
+create index fec_indiv_allcycles_filer_id on fec_indiv_allcycles (filer_id);
+
+
+create view fec_pac2cand_allcycles_import as
+    select 2012 as cycle, * from fec_pac2cand_12
+union all
+    select 2010, * from fec_pac2cand_10
+union all
+    select 2008, * from fec_pac2cand_08
+union all
+    select 2006, * from fec_pac2cand_06
+union all
+    select 2004, * from fec_pac2cand_04
+union all
+    select 2002, * from fec_pac2cand_02
+union all
+    select 2000, * from fec_pac2cand_00;
+
+drop table if exists fec_pac2cand_allcycles;
+create table fec_pac2cand_allcycles as
+select cycle, fec_record, filer_id, transaction_type, 
+    (transaction_century || transaction_year || transaction_month || transaction_day)::date as date,
+    overpunch(amount) as amount,
+    other_id, candidate_id
+from fec_pac2cand_allcycles_import;
+
+create index fec_pac2cand_allcycles_filer_id on fec_pac2cand_allcycles (filer_id);
+create index fec_pac2cand_allcycles_other_id on fec_pac2cand_allcycles (other_id);
+create index fec_pac2cand_allcycles_candidate_id on fec_pac2cand_allcycles (candidate_id);
+    
+
+create view fec_pac2pac_allcycles_import as
+    select 2012 as cycle, * from fec_pac2pac_12
+union all
+    select 2010, * from fec_pac2pac_10
+union all
+    select 2008, * from fec_pac2pac_08
+union all
+    select 2006, * from fec_pac2pac_06
+union all
+    select 2004, * from fec_pac2pac_04
+union all
+    select 2002, * from fec_pac2pac_02
+union all
+    select 2000, * from fec_pac2pac_00;
+
+drop table if exists fec_pac2pac_allcycles;
+create table fec_pac2pac_allcycles as
+select cycle, fec_record, filer_id, transaction_type, contributor_lender_transfer as contributor_name, city, state, zipcode, occupation, 
+    (transaction_century || transaction_year || transaction_month || transaction_day)::date as date,
+    overpunch(amount) as amount,
+    other_id
+from fec_pac2pac_allcycles_import;
+
+create index fec_pac2pac_allcycles_filer_id on fec_pac2pac_allcycles (filer_id);
+create index fec_pac2pac_allcycles_other_id on fec_pac2pac_allcycles (other_id);
+
+
 create table fec_committee_summaries_allcycles_import as
     select 2012 as cycle, * from fec_pac_summaries_12
     union all
@@ -30,6 +152,7 @@ from fec_committee_summaries_allcycles_import
 where
     -- there are a number of data-less rows. Discard them by looking for valid date.
     through_year != 0;
+
 
 
 
