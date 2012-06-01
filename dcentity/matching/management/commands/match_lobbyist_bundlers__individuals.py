4d2710b4e73df7d7a34b8c58f8eabd9c2ce83351
Alison Rowland <arowland@sunlightfoundation.com>
2012-06-01 11:45:20 -0400

    Some lobbyist bundling loading and matching changes

--- dcentity/matching/management/commands/match_lobbyist_bundlers__individuals.py
+++ dcentity/matching/management/commands/match_lobbyist_bundlers__individuals.py
@@ -8,10 +8,12 @@ class Command(MatchingCommand):
 
     def __init__(self, *args, **kwargs):
         super(Command, self).__init__(*args, **kwargs)
-        self.subject = LobbyistBundle.objects.exclude(name=F('employer'))
+        self.subject = LobbyistBundle.objects.exclude(name=F('employer')).extra(where=['name not in (select name from assoc_bundler_matches_manual)']).order_by('name')
         self.subject_name_attr = 'name'
         self.subject_id_type = 'integer'
 
-        self.match = Entity.objects.filter(type='individual').extra(where=['id in (select entity_id from assoc_lobbying_lobbyist)'])
+        self.match = Entity.objects.filter(type='individual').extra(where=[
+            'id in (select entity_id from assoc_lobbying_lobbyist)',
+        ])
         self.match_table_prefix = 'bundler_indiv'
 
