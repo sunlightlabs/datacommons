d92a7a345a9ccd65da9e804643cd0c643ef46f17
Alison Rowland <arowland@sunlightfoundation.com>
2012-06-22 15:18:57 -0400

    [#1084] Making the importer scripts pass in their own set of emails

--- dcdata/management/base/nimsp_importer.py
+++ dcdata/management/base/nimsp_importer.py
@@ -1,8 +1,10 @@
 from dcdata.management.base.importer import BaseImporter
+from settings import NIMSP_IMPORT_LOG_EMAIL
 
 
 class BaseNimspImporter(BaseImporter):
     email_subject = 'Unhappy NIMSP Loading App'
+    email_recipients = NIMSP_IMPORT_LOG_EMAIL
 
     def __init__(self):
         super(BaseNimspImporter, self).__init__()
