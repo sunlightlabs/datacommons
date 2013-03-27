0cabccd9dc3ae3ff5402bbe089e26ebbcfbd07c2
Alison Rowland <arowland@sunlightfoundation.com>
2013-03-27 17:41:32 -0400

    Cleaning up allcycles and other unused FEC files

--- dcdata/fec/allcycles/preload.sql
+++
@@ -1,7 +0,0 @@
-drop view if exists fec_candidates_allcycles_import;
-drop view if exists fec_committees_allcycles cascade;
-drop view if exists fec_indiv_allcycles_import;
-drop view if exists fec_pac2cand_allcycles_import;
-drop view if exists fec_pac2pac_allcycles_import;
-drop view if exists fec_committee_summaries_allcycles_import;
-drop view if exists fec_candidate_summaries_allcycles_import;
