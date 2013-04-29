5906619f23fc62313ab6b728a4e5dcdde001d049
Alison Rowland <arowland@sunlightfoundation.com>
2013-04-29 16:00:17 -0400

    Fix Jim Marshall's name spelling

---
+++ sql_updates/1211_fix_jim_marshalls_name.sql
@@ -0,0 +1,2 @@
+update matchbox_entity set name = 'James Creel Marshall (D)' where id = '9ac1df4c-c628-4e92-b9f2-1b090d7caa0e';
+update matchbox_entityalias set alias = 'James Creel Marshall (D)' where id = 46965;
