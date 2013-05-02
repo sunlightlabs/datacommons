cfc0e7d6d25058202db3a3e1bc99d3471d049d3c
Alison Rowland <arowland@sunlightfoundation.com>
2013-05-02 14:04:03 -0400

    Updating Locksmith to use the new hub URL

--- settings/defaults.py
+++ settings/defaults.py
@@ -92,7 +92,7 @@ PISTON_DISPLAY_ERRORS = True
 PISTON_EMAIL_ERRORS = False
 PISTON_STREAM_OUTPUT = True
 
-LOCKSMITH_HUB_URL = "http://services.sunlightlabs.com/analytics/"
+LOCKSMITH_HUB_URL = "http://sunlightfoundation.com/api/analytics/"
 LOCKSMITH_HTTP_HEADER = None
 LOCKSMITH_LOG_PATH = '/var/log/nginx/dc_web_access.log'
 
