b4d2ab4f0473dfa02426af942000a1b0dc5aaa36
Bob Lannon <blannon@gmail.com>
2013-05-22 01:16:48 -0400

    fixing summary to ensure that top 10 are across all third party categories. this is lossy, but necessary for the landing page charts

--- dcapi/aggregates/contributions/contribution_summaries.sql
+++ dcapi/aggregates/contributions/contribution_summaries.sql
@@ -7,21 +7,28 @@ drop table if exists summary_party_from_biggest_org;
 
 select date_trunc('second', now()) || ' -- create table summary_party_from_biggest_org';
 create table summary_party_from_biggest_org as
-    
-        select 
-            organization_entity, 
-            me.name, 
-            recipient_party, 
+
+        select
+            organization_entity, name, recipient_party, cycle,
+            sum(count) as count,
+            sum(amount) as amount,
+            rank() over(partition by recipient_party,cycle order by sum(amount) desc) as rank
+        from
+        (select
+            organization_entity,
+            me.name,
+            case when recipient_party in ('D','R') then recipient_party else 'Other' end as recipient_party,
             cycle,
-            count, 
-            amount, 
-            rank() over(partition by recipient_party,cycle order by amount desc) as rank 
-        from 
-                matchbox_entity me 
-            inner join 
-                agg_party_from_org apfo on me.id = apfo.organization_entity 
-        where 
-            exists  (select 1 from biggest_organization_associations boa where apfo.organization_entity = boa.entity_id);
+            count,
+            amount
+        from
+                matchbox_entity me
+            inner join
+                agg_party_from_org apfo on me.id = apfo.organization_entity
+        where
+            exists  (select 1 from biggest_organization_associations boa where apfo.organization_entity = boa.entity_id)
+            ) three_party
+        group by organization_entity, name, recipient_party, cycle;
 
 select date_trunc('second', now()) || ' -- Vcreate index summary_party_from_biggest_org_idx on summary_party_from_biggest_org (organization_entity, cycle)';
 create index summary_party_from_biggest_org_idx on summary_party_from_biggest_org (organization_entity, recipient_party, cycle);
