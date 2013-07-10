f3df4fd9471fe84c79f659098a224972a1938894
Bob Lannon <blannon@gmail.com>
2013-07-10 12:11:21 -0400

    trying new indexes for speed

--- dcapi/aggregates/lobbying/lobbying_summaries.sql
+++ dcapi/aggregates/lobbying/lobbying_summaries.sql
@@ -38,7 +38,7 @@
   ;
   
   select date_trunc('second', now()) || ' -- create index summary_lobbying_issues_for_biggest_org_idx on summary_lobbying_issues_for_biggest_org (client_entity, cycle)';
-  create index summary_lobbying_top_issues_for_biggest_orgs_idx on summary_lobbying_top_issues_for_biggest_orgs (issue, cycle, rank_by_amount);
+  create index summary_lobbying_top_issues_for_biggest_orgs_idx on summary_lobbying_top_issues_for_biggest_orgs (cycle, rank_by_amount);
 
  -- Ranking Biggest Parentmost Clients for Lobbying Issues by Amount and Count
  
@@ -83,7 +83,7 @@
   ;
   
   select date_trunc('second', now()) || ' -- create index summary_lobbying_issues_for_biggest_org_idx on summary_lobbying_issues_for_biggest_org (client_entity, cycle)';
-  create index summary_lobbying_top_biggest_orgs_for_issue_idx on summary_lobbying_top_biggest_orgs_for_issue (issue, client_entity, cycle);
+  create index summary_lobbying_top_biggest_orgs_for_issue_idx on summary_lobbying_top_biggest_orgs_for_issue (issue, cycle);
 
 
  
