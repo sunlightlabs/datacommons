27c2b8953d57dd3a227827090b1ca36c0e64665b
Bob Lannon <blannon@gmail.com>
2013-11-04 12:56:41 -0500

    linking charlie crist to his state profile

---
+++ dcdata/scripts/one_time/2013-11-04-state_fed_manual_edits.sql
@@ -0,0 +1,7 @@
+begin;
+
+insert into matchbox_statefederal (state_politician_entity_id, federal_politician_entity_id)
+values
+    ('b792a35d3d364b14982cf960662016e9', '0fc3d891b38a43abaa0a3ec32c0ad0cf'),
+    
+commit;
