c8f3c65662bb5bb208cf06c3ae16613d12929981
Bob Lannon <blannon@gmail.com>
2013-11-04 12:08:50 -0500

    re-added feedinator

--- settings/defaults.py
+++ settings/defaults.py
@@ -65,6 +65,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'feedinator',
     'mediasync',
     'locksmith.auth',
     'locksmith.logparse',
