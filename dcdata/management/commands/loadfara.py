8b3813dca8eb63676cf60f4e7db65ab738888031
Bob Lannon <blannon@gmail.com>
2014-04-06 18:56:08 -0400

    getting rid of self refs

--- dcdata/management/commands/loadfara.py
+++ dcdata/management/commands/loadfara.py
@@ -47,7 +47,7 @@ def execute_sql_file(cursor, filename):
     contents = " ".join([line for line in open(filename, 'r') if line[0:2] != '--'])
     statements = contents.split(';')[:-1]  # split on semi-colon. Last element will be trailing whitespace
     for statement in statements:
-        self.log.info("Executing %s" % statement)
+        sys.stdout.write("Executing %s" % statement)
         cursor.execute(statement)
 
 def parse_fara_date(date_str):
